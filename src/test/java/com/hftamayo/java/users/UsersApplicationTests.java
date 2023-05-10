52e995aec1862e74e9a116afff85e02e39adab0d
hftamayo <hftamayo@gmail.com>
2023-05-10 17:33:32 -0600

    project starting point

---
+++ src/test/java/com/hftamayo/java/users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hftamayo.java.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
