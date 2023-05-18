3d407bc70956c4b061c8f55171d2811ef6b0cc66
hftamayo <hftamayo@gmail.com>
2023-05-18 10:03:25 -0600

    adding jpa support to the project

--- src/main/java/com/hftamayo/java/todo/Repository/TodoInterface.java
+++ src/main/java/com/hftamayo/java/todo/Repository/TodoInterface.java
@@ -1,6 +1,7 @@
 package com.hftamayo.java.todo.Repository;
 
 import com.hftamayo.java.todo.Model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TodoInterface extends JpaRepository <Task, Long>{
+public interface TodoInterface extends JpaRepository<Task, Long> {
 }
