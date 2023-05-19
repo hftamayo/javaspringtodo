934e2b381f6d7b7c26d4582b97876eca6b26dee0
hftamayo <hftamayo@gmail.com>
2023-05-19 10:24:44 -0600

    first approach to saveTask method

--- src/main/java/com/hftamayo/java/todo/Controller/TodoController.java
+++ src/main/java/com/hftamayo/java/todo/Controller/TodoController.java
@@ -2,13 +2,18 @@ package com.hftamayo.java.todo.Controller;
 
 import com.hftamayo.java.todo.Model.Task;
 import com.hftamayo.java.todo.Repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 public class TodoController {
+    @Autowired
+    private TodoRepository todoRepository;
     @GetMapping(value = "/saludo")
     public String holaMundo(){
         return "Hola Mundo";
@@ -16,6 +21,12 @@ public class TodoController {
 
     @GetMapping(value = "/tasks")
     public List<Task> getTasks(){
-        return TodoRepository.findAll();
+        return todoRepository.findAll();
+    }
+
+    @PostMapping(value = "/savetask")
+    public String saveTask(@RequestBody Task task){
+        todoRepository.save(task);
+        return "Task saved";
     }
 }
