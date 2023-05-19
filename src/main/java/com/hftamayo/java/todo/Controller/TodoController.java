72136858de615e0c39e41966ab428e6b1368dddd
hftamayo <hftamayo@gmail.com>
2023-05-19 10:02:44 -0600

    refactoring repo's name and adding a new endpoint part1

--- src/main/java/com/hftamayo/java/todo/Controller/TodoController.java
+++ src/main/java/com/hftamayo/java/todo/Controller/TodoController.java
@@ -1,12 +1,21 @@
 package com.hftamayo.java.todo.Controller;
 
+import com.hftamayo.java.todo.Model.Task;
+import com.hftamayo.java.todo.Repository.TodoRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class TodoController {
     @GetMapping(value = "/saludo")
     public String holaMundo(){
         return "Hola Mundo";
+    }
+
+    @GetMapping(value = "/tasks")
+    public List<Task> getTasks(){
+        return TodoRepository.findAll();
     }
 }
