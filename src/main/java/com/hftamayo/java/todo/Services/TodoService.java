626650b98b0d6f443f8f2660c60129d52d3b12cb
hftamayo <hftamayo@gmail.com>
2023-05-25 11:05:36 -0600

    wiring repository with service classes

--- src/main/java/com/hftamayo/java/todo/Services/TodoService.java
+++ src/main/java/com/hftamayo/java/todo/Services/TodoService.java
@@ -2,12 +2,20 @@ package com.hftamayo.java.todo.Services;
 
 import com.hftamayo.java.todo.Model.Task;
 import com.hftamayo.java.todo.Repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class TodoService {
 
-    private TodoRepository todoRepository;
+    private final TodoRepository todoRepository;
+
+    @Autowired
+    public TodoService(TodoRepository todoRepository){
+        this.todoRepository = todoRepository;
+    }
 
     public List<Task> getTasks(){
         return todoRepository.findAll();
