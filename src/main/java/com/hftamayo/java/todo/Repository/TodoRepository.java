b29b2393f0a11c367d4440487245765a9782bfec
hftamayo <hftamayo@gmail.com>
2023-05-29 11:38:31 -0600

    adding a Repository annotation

--- src/main/java/com/hftamayo/java/todo/Repository/TodoRepository.java
+++ src/main/java/com/hftamayo/java/todo/Repository/TodoRepository.java
@@ -2,6 +2,8 @@ package com.hftamayo.java.todo.Repository;
 
 import com.hftamayo.java.todo.Model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TodoRepository extends JpaRepository<Task, Long> {
 }
