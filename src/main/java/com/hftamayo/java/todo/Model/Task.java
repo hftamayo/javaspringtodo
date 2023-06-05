80ce9eaebe2bb9267a6136cbae20132512c31ad1
hftamayo <hftamayo@gmail.com>
2023-06-05 09:20:57 -0600

    cleaning the model class of unneeded code

--- src/main/java/com/hftamayo/java/todo/Model/Task.java
+++ src/main/java/com/hftamayo/java/todo/Model/Task.java
@@ -3,7 +3,6 @@ package com.hftamayo.java.todo.Model;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
-import java.time.Period;
 import java.time.temporal.ChronoUnit;
 
 @Entity
@@ -25,14 +24,6 @@ public class Task {
 
     @Column(nullable = false)
     private LocalDate dateUpdated;
-
-    @Transient
-    private long daysAdded;
-
-    public Task(String title, String description) {
-        this.title = title;
-        this.description = description;
-    }
 
     public Task() {
 
