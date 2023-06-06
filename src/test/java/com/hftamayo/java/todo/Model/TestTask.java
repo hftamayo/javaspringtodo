3b470849e67a204e10e08e3da954d610617f5562
hftamayo <hftamayo@gmail.com>
2023-06-06 15:58:37 -0600

    using the function assertEquals

--- src/test/java/com/hftamayo/java/todo/Model/TestTask.java
+++ src/test/java/com/hftamayo/java/todo/Model/TestTask.java
@@ -3,7 +3,7 @@ package com.hftamayo.java.todo.Model;
 import org.assertj.core.api.Assert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.time.LocalDate;
 
 public class TestTask {
@@ -12,13 +12,13 @@ public class TestTask {
     @DisplayName("Date Added value on Create")
     public void givenProperties_whenNewTask_thenCheckDateAdded(){
         Task testTask = new Task("Supermarket List", "meat, cereal, cheese and milk");
-        Assert.equals(LocalDate.now(), testTask.getDateAdded());
+        assertEquals(LocalDate.now(), testTask.getDateAdded());
     }
 
     @Test
     @DisplayName("Date Updated value on Create")
     public void givenProperties_whenNewTask_thenCheckDateUpdated(){
         Task testTask = new Task("Supermarket List", "meat, cereal, cheese and milk");
-        Assert.equals(LocalDate.now(), testTask.getDateUpdated());
+        assertEquals(LocalDate.now(), testTask.getDateUpdated());
     }    
 }
