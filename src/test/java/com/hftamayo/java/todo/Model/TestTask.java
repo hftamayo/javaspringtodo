71f8849429cda8477f1760eb54b9ff32d4daecab
hftamayo <hftamayo@gmail.com>
2023-06-07 15:09:58 -0600

    first approaches on duplicate records and getDaysAdded function

--- src/test/java/com/hftamayo/java/todo/Model/TestTask.java
+++ src/test/java/com/hftamayo/java/todo/Model/TestTask.java
@@ -45,4 +45,22 @@ public class TestTask {
         newTask.onUpdate();
         assertEquals(LocalDate.now(), newTask.getDateUpdated());
     }
+
+    @Test
+    @DisplayName("Reject insert existing task")
+    public void givenNewTitle_whenAlreadyExists_thenRejectTaskCreation(){
+        Task testTask = new Task();
+        testTask.setTitle("Supermarket list");
+    }
+
+    @Test
+    @DisplayName("Get Days Added")
+    public void givenDisplayDaysAdded_whenGetDaysAddedRequest_thenDisplayGetDaysAdded(){
+        final LocalDate FAKE_DATE_ADDED = LocalDate.of(2023, 03, 01);
+        final LocalDate FAKE_DATE_NOW  = LocalDate.of(2023, 06, 01);
+        when(LocalDate.now()).thenReturn(FAKE_DATE_NOW);
+        Task testTask = new Task();
+        testTask.onCreate();
+        LocalDate fakeDateNow = LocalDate.parse("01-06-2023");
+    }
 }
