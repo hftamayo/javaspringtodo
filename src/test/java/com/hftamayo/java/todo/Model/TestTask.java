61bba5f260e3e07c14a68c0c5c6a21e18a6406d9
hftamayo <hftamayo@gmail.com>
2023-06-07 13:17:31 -0600

    basic methods testing accepting string values and rejecting nulls

--- src/test/java/com/hftamayo/java/todo/Model/TestTask.java
+++ src/test/java/com/hftamayo/java/todo/Model/TestTask.java
@@ -4,21 +4,29 @@ import org.assertj.core.api.Assert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import java.time.LocalDate;
 
 public class TestTask {
 
     @Test
-    @DisplayName("Date Added value on Create")
-    public void givenProperties_whenNewTask_thenCheckDateAdded(){
-        Task testTask = new Task("Supermarket List", "meat, cereal, cheese and milk");
-        assertEquals(LocalDate.now(), testTask.getDateAdded());
+    @DisplayName("New Task: not null fields")
+    public void givenValues_whenNewTask_thenAcceptStringFields(){
+        Task testTask = new Task();
+        testTask.setTitle("Supermarket list");
+        testTask.setDescription("meat, cereal, cheese and milk");
+        assertEquals("Supermarket list", testTask.getTitle());
+        assertEquals("meat, cereal, cheese and milk", testTask.getDescription());
     }
 
     @Test
-    @DisplayName("Date Updated value on Create")
-    public void givenProperties_whenNewTask_thenCheckDateUpdated(){
-        Task testTask = new Task("Supermarket List", "meat, cereal, cheese and milk");
-        assertEquals(LocalDate.now(), testTask.getDateUpdated());
-    }    
+    @DisplayName("New Task: not null fields")
+    public void givenNullValues_whenNewTask_thenRejectNulls(){
+        Task testTask = new Task();
+        testTask.setTitle("");
+        testTask.setDescription("");
+        assertNotNull(testTask.getTitle());
+        assertNotNull(testTask.getDescription());
+    }
 }
