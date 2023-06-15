141c2663bb292ad9ffc53ff197e7c4f8f79076e9
hftamayo <hftamayo@gmail.com>
2023-06-15 10:50:40 -0600

    unit testing for todoRepo part1

--- src/test/java/com/hftamayo/java/todo/Repository/TodoRepositoryTest.java
+++ src/test/java/com/hftamayo/java/todo/Repository/TodoRepositoryTest.java
@@ -1,4 +1,27 @@
 package com.hftamayo.java.todo.Repository;
 
+import com.hftamayo.java.todo.Model.Task;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Arrays;
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
+@DataJpaTest
+
 public class TodoRepositoryTest {
+    @Autowired
+    private TodoRepository todoRepository;
+
+    @BeforeEach
+    void initUseCase(){
+        List<Task> tasks = Arrays.asList(
+                new Task("supermarket list", "milk, cheese, fruits, veggies")
+        );
+        todoRepository.saveAll(tasks);
+    }
 }
