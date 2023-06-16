9fd939e1afc363c1eb15866b8071cb967d2fddb8
hftamayo <hftamayo@gmail.com>
2023-06-16 08:48:53 -0600

    saveAll test method

--- src/test/java/com/hftamayo/java/todo/Repository/TodoRepositoryTest.java
+++ src/test/java/com/hftamayo/java/todo/Repository/TodoRepositoryTest.java
@@ -3,6 +3,7 @@ package com.hftamayo.java.todo.Repository;
 import com.hftamayo.java.todo.Model.Task;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,9 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(MockitoExtension.class)
 @DataJpaTest
@@ -29,5 +33,23 @@ public class TodoRepositoryTest {
     @AfterEach
     public void destroyAll(){
         todoRepository.deleteAll();
+    }
+
+    @Test
+    void saveAll_success(){
+        List<Task> tasks = Arrays.asList(
+                new Task("pay the bills", "water, cable, electricity"),
+                new Task("family appointment", "breakfast or lunch"),
+                new Task("golang project", "keep working on it")
+        );
+        Iterable<Task> allTasks = todoRepository.saveAll(tasks);
+
+        AtomicInteger validIdFound = new AtomicInteger();
+        allTasks.forEach(task -> {
+            if(task.getId()>0){
+                validIdFound.getAndIncrement();
+            }
+        });
+        assertThat(validIdFound.intValue()).isEqualTo(3);
     }
 }
