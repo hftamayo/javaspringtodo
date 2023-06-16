8be46c62efb582fad76be32a114b72ccf3852f20
hftamayo <hftamayo@gmail.com>
2023-06-16 09:40:28 -0600

    first approach to TodoController Test

--- src/test/java/com/hftamayo/java/todo/Controller/TodoControllerTest.java
+++ src/test/java/com/hftamayo/java/todo/Controller/TodoControllerTest.java
@@ -1,4 +1,19 @@
 package com.hftamayo.java.todo.Controller;
 
+import com.hftamayo.java.todo.Services.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest({TodoController.class})
 public class TodoControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    TodoService todoService;
+
 }
