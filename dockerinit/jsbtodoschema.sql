b1dcf14b6182f1edb3352911f35df04e615b4c83
hftamayo <hftamayo@gmail.com>
2023-08-30 11:16:28 -0600

    rearranging docker objects related

---
+++ dockerinit/jsbtodoschema.sql
@@ -0,0 +1,6 @@
+CREATE USER 'hftamayo'@'localhost' IDENTIFIED BY 'db43uskAdY11';
+GRANT ALL PRIVILEGES ON * . * TO 'hftamayo'@'localhost'; 
+CREATE USER 'jsbtodo'@'localhost' IDENTIFIED BY 'jsbtodo123$';
+CREATE DATABASE jsbtodo;
+GRANT ALL PRIVILEGES ON jsbtodo.* TO 'jsbtodo'@'localhost';
+FLUSH PRIVILEGES;
