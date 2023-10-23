5cdb3f69b43446489e5774912cf2128820e60c8d
hftamayo <hftamayo@gmail.com>
2023-10-23 15:33:04 -0600

    task model test routines

--- src/test/java/com/hftamayo/java/todo/Model/TaskTest.java
+++ src/test/java/com/hftamayo/java/todo/Model/TaskTest.java
@@ -1,83 +1,80 @@
-//package com.hftamayo.java.todo.Model;
-//
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.MockedStatic;
-//import org.mockito.Mockito;
-//
-//import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.junit.jupiter.api.Assertions.assertNotNull;
-//
-//import java.time.*;
-//
-//public class TaskTest {
-//    MockedStatic<LocalDateTime> localDateTimeMocked;
-//
-//    @Test
-//    @DisplayName("New Task: sending valid values")
-//    public void givenValues_whenNewTask_thenAcceptStringFields(){
-//        Task testTask = new Task();
-//        testTask.setTitle("Supermarket list");
-//        testTask.setDescription("meat, cereal, cheese and milk");
-//        assertEquals("Supermarket list", testTask.getTitle());
-//        assertEquals("meat, cereal, cheese and milk", testTask.getDescription());
-//    }
-//
-//    @Test
-//    @DisplayName("New Task: rejecting null values")
-//    public void givenNullValues_whenNewTask_thenRejectNulls(){
-//        Task testTask = new Task();
-//        testTask.setTitle("");
-//        testTask.setDescription("");
-//        assertNotNull(testTask.getTitle());
-//        assertNotNull(testTask.getDescription());
-//    }
-//
-//    @Test
-//    @DisplayName("New task: checking dateAdded value")
-//    public void givenTaskCreationRequest_whenOnCreate_thenCheckDatedAdded(){
-//        Task newTask = new Task();
-//        newTask.onCreate();
-//        assertEquals(LocalDate.now(), newTask.getDateAdded().toLocalDate());
-//    }
-//
-//    @Test
-//    @DisplayName("New task: checking updateAdded value")
-//    public void givenTaskUpdateRequest_whenOnUpdate_thenCheckDatedUpdated(){
-//        Task newTask = new Task();
-//        newTask.onUpdate();
-//        assertEquals(LocalDate.now(), newTask.getDateUpdated().toLocalDate());
-//    }
-//
-//    @Test
-//    @DisplayName("Reject insert existing task's title")
-//    public void givenNewTitle_whenAlreadyExists_thenRejectTaskCreation(){
-//        Task firstTestTask = new Task();
-//        firstTestTask.setTitle("Supermarket list");
-//        firstTestTask.setDescription("meat, cereal, cheese and milk");
-//        Task secondTestTask = new Task();
-//        secondTestTask.setTitle("Supermarket list");
-//        secondTestTask.setDescription("fruits, veggies");
-//        //please polish the following case evaluation:
-//        Assertions.assertFalse(firstTestTask.getTitle() != secondTestTask.getTitle());
-//
-//    }
-//
-//    @Test
-//    @DisplayName("Get Days Added")
-//    public void givenNewTask_whenGetDaysAddedRequest_thenDisplayHowManyDaysAdded(){
-//        localDateTimeMocked = Mockito.mockStatic(LocalDateTime.class, Mockito.CALLS_REAL_METHODS);
-//        LocalDateTime fakeDateAdded = LocalDateTime.of(2023, 03, 01, 12, 0);
-//        localDateTimeMocked.when(LocalDateTime::now).thenReturn(fakeDateAdded);
-//
-//        Task testTask = new Task();
-//        testTask.onCreate();
-//
-//        LocalDateTime fakeDateNow = LocalDateTime.of(2023, 06, 01, 12, 0);
-//        localDateTimeMocked.when(LocalDateTime::now).thenReturn(fakeDateNow);
-//
-//        assertEquals(testTask.getDaysAdded(), 92);
-//    }
-//}
+package com.hftamayo.java.todo.Model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.time.LocalDateTime;
+
+class TaskTest {
+
+    private Task task;
+
+    @BeforeEach
+    void setUp() {
+        task = new Task();
+        task.onCreate();
+    }
+
+    @Test
+    @DisplayName("Class Instantiation")
+    void testTaskInstantiation() {
+        assertNotNull(task);
+        assertEquals(0, task.getId());
+        assertNull(task.getTitle());
+        assertNull(task.getDescription());
+        assertNotNull(task.getDateAdded());
+        assertNotNull(task.getDateUpdated());
+        assertTrue(task.isStatus());
+    }
+
+    @Test
+    @DisplayName("Class setters and getters")
+    void testGetterAndSetterMethods() {
+        task.setId(1L);
+        task.setTitle("Sample Task");
+        task.setDescription("Task Description");
+        task.setStatus(false);
+
+        assertEquals(1L, task.getId());
+        assertEquals("Sample Task", task.getTitle());
+        assertEquals("Task Description", task.getDescription());
+        assertFalse(task.isStatus());
+    }
+
+    @Test
+    @DisplayName("getDaysAdded function")
+    void testGetDaysAdded() {
+        LocalDateTime dateAdded = LocalDateTime.now().minusDays(2);
+        task.setDateAdded(dateAdded);
+
+        long daysAdded = task.getDaysAdded();
+        assertEquals(2, daysAdded);
+    }
+
+    @Test
+    @DisplayName("Operations before save record or update record")
+    void testPrePersistAndUpdateMethods() {
+        Task spyTask = spy(task);
+
+        doNothing().when(spyTask).onCreate();
+        doNothing().when(spyTask).onUpdate();
+
+        spyTask.onCreate();
+        assertNotNull(spyTask.getDateAdded());
+        assertNotNull(spyTask.getDateUpdated());
+
+        spyTask.onUpdate();
+        assertNotNull(spyTask.getDateUpdated());
+    }
+}
+
+
+
+
+
