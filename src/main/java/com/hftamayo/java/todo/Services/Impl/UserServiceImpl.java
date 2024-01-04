e9b4fd47fd370aea1e816518352383dcea29f762
hftamayo <hftamayo@gmail.com>
2024-01-04 14:41:32 -0600

    coding User Service Implementation class part 1

---
+++ src/main/java/com/hftamayo/java/todo/Services/Impl/UserServiceImpl.java
@@ -0,0 +1,73 @@
+package com.hftamayo.java.todo.Services.Impl;
+
+import com.hftamayo.java.todo.Model.User;
+import com.hftamayo.java.todo.Repository.UserRepository;
+import com.hftamayo.java.todo.Services.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getUsers(){
+        return userRepository.findAll();
+    }
+
+    public List<User> getAllUsersByStatus(boolean isActive){
+        return userRepository.findAllByActive(isActive);
+    }
+
+    public User getUserById(long userId){
+        return userRepository.findById(userId).get();
+    }
+
+    public User getUserByUsername(String username){
+        return userRepository.findByUsername(username);
+    }
+
+    public User getUserByEmail(String email){
+        return userRepository.findByEmail(email);
+    }
+
+    public User getUserByUsernameAndPassword(String username, String password){
+        return userRepository.findByUsernameAndPassword(username, password);
+    }
+
+    public User getUserByEmailAndPassword(String email, String password){
+        return userRepository.findByEmailAndPassword(email, password);
+    }
+
+    public long countAllUserByUsername(String username){
+        return userRepository.countAllByUsername(username);
+    }
+
+    public long countAllUserByEmail(String email){
+        return userRepository.countAllByEmail(email);
+    }
+
+    @Override
+    public User saveUser(User newUser) {
+        User requestedUser = userRepository.findByEmail(newUser.getEmail());
+        if(requestedUser == null){
+            return userRepository.save(newUser);
+        } else {
+            throw new RuntimeException("User already exists");
+        }
+    }
+
+    @Override
+    public User updateUser(long userId, User updatedUser) {
+        return null;
+    }
+
+    @Override
+    public void deleteUser(long userId) {
+
+    }
+}
