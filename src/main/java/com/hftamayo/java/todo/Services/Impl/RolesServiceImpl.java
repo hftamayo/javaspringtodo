5c06e2504bb56e17380674f3f32a0a28f7f511f6
hftamayo <hftamayo@gmail.com>
2024-01-16 15:31:17 -0600

    first version of Roles Service Implementation

---
+++ src/main/java/com/hftamayo/java/todo/Services/Impl/RolesServiceImpl.java
@@ -0,0 +1,50 @@
+package com.hftamayo.java.todo.Services.Impl;
+
+import com.hftamayo.java.todo.Model.Roles;
+import com.hftamayo.java.todo.Repository.RolesRepository;
+import com.hftamayo.java.todo.Services.RolesService;
+
+public class RolesServiceImpl implements RolesService {
+    private final RolesRepository rolesRepository;
+
+    public RolesServiceImpl(RolesRepository rolesRepository) {
+        this.rolesRepository = rolesRepository;
+    }
+
+    public Roles getRoleByName(String name){
+        return rolesRepository.findByRoleName(name);
+    }
+
+    @Override
+    public Roles saveRole(Roles newRole) {
+        Roles requestedRole = rolesRepository.findByRoleName(newRole.getName());
+        if(requestedRole == null){
+            return rolesRepository.save(newRole);
+        } else {
+            throw new RuntimeException("Role already exists");
+        }
+    }
+
+    @Override
+    public Roles updateRole(long roleId, Roles updatedRole) {
+        Roles requestedRole = rolesRepository.findById(roleId).get();
+        if(requestedRole != null){
+            requestedRole.setName(updatedRole.getName());
+            requestedRole.setDescription(updatedRole.getDescription());
+            requestedRole.setStatus(updatedRole.isStatus());
+            return rolesRepository.save(requestedRole);
+        } else {
+            throw new RuntimeException("Role not found");
+        }
+    }
+
+    @Override
+    public void deleteRole(long roleId) {
+        Roles requestedRole = rolesRepository.findById(roleId).get();
+        if(requestedRole != null){
+            rolesRepository.delete(requestedRole);
+        } else {
+            throw new RuntimeException("Role not found");
+        }
+    }
+}
