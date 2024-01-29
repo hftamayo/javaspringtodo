21e742a23753f1c32807e59672ff0573786b07e4
hftamayo <hftamayo@gmail.com>
2024-01-29 14:55:44 -0600

    AuthFilter and CustomUserDets have bugs to fix

---
+++ src/main/java/com/hftamayo/java/todo/Security/CustomUserDetailsService.java
@@ -0,0 +1,36 @@
+package com.hftamayo.java.todo.Security;
+
+import com.hftamayo.java.todo.Model.User;
+import com.hftamayo.java.todo.Repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+        User user = userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found by Name or Email using this criteria: " + usernameOrEmail));
+
+        Set<GrantedAuthority> authorities = user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toSet());
+
+        return new org.springframework.security.core.userdetails.User(
+                usernameOrEmail,
+                user.getPassword(),
+                authorities
+        );
+    }
+}
--- src/main/java/com/hftamayo/java/todo/Security/JwtAuthenticationFilter.java
+++ src/main/java/com/hftamayo/java/todo/Security/JwtAuthenticationFilter.java
@@ -46,6 +46,4 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         }
         return null;
     }
-
-
 }
