7dc80bd3c51b7c00b34d5fe1094f4c1bc88e7bf1
hftamayo <hftamayo@gmail.com>
2024-01-30 15:54:54 -0600

    Authentication classes workflow

---
+++ src/main/java/com/hftamayo/java/todo/Controller/AuthController.java
@@ -0,0 +1,27 @@
+package com.hftamayo.java.todo.Controller;
+
+import com.hftamayo.java.todo.Dto.LoginDto;
+import com.hftamayo.java.todo.Security.JwtAuthResponse;
+import com.hftamayo.java.todo.Services.AuthService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    private AuthService authService;
+    @PostMapping("/login")
+    public ResponseEntity<JwtAuthResponse> authenticate(@RequestBody LoginDto loginDto) {
+        String token = authService.login(loginDto);
+
+        JwtAuthResponse jwtAuthResponse = new JwtAuthResponse();
+        jwtAuthResponse.setAccessToken(token);
+
+        return ResponseEntity.ok(jwtAuthResponse);
+    }
+}
---
+++ src/main/java/com/hftamayo/java/todo/Services/AuthService.java
@@ -0,0 +1,7 @@
+package com.hftamayo.java.todo.Services;
+
+import com.hftamayo.java.todo.Dto.LoginDto;
+
+public interface AuthService {
+    String login(LoginDto loginDto);
+}
