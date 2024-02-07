ad1165346f00d98501cdd0b2041897aa9c623671
hftamayo <hftamayo@gmail.com>
2024-02-07 15:02:22 -0600

    hashing password of the register user with BCrypt

--- src/main/java/com/hftamayo/java/todo/Services/Impl/UserServiceImpl.java
+++ src/main/java/com/hftamayo/java/todo/Services/Impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.hftamayo.java.todo.Services.Impl;
 import com.hftamayo.java.todo.Model.User;
 import com.hftamayo.java.todo.Repository.UserRepository;
 import com.hftamayo.java.todo.Services.UserService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,9 +12,11 @@ import java.util.Optional;
 @Service
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public List<User> getUsers(){
@@ -56,6 +59,7 @@ public class UserServiceImpl implements UserService {
     public User saveUser(User newUser) {
         Optional<User> requestedUser = userRepository.findByEmail(newUser.getEmail());
         if(requestedUser == null){
+            newUser.setPassword(passwordEncoder.encode(newUser.getPassword()));
             return userRepository.save(newUser);
         } else {
             throw new RuntimeException("User already exists");
