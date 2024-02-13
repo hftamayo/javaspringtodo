713b10c484773282c69346d28373c7d18413e0eb
hftamayo <hftamayo@gmail.com>
2024-02-13 12:27:46 -0600

    updating authService with methods related to logout

--- src/main/java/com/hftamayo/java/todo/Services/AuthService.java
+++ src/main/java/com/hftamayo/java/todo/Services/AuthService.java
@@ -1,7 +1,10 @@
 package com.hftamayo.java.todo.Services;
 
 import com.hftamayo.java.todo.Dto.LoginDto;
+import jakarta.servlet.http.HttpServletRequest;
 
 public interface AuthService {
     String login(LoginDto loginDto);
+    String extractTokenFromRequest(HttpServletRequest request);
+    void invalidateToken(String token);
 }
