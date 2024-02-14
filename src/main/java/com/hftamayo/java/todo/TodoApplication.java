e991868651cbe8e55e40987fc6d396b6de0bdb65
hftamayo <hftamayo@gmail.com>
2024-02-14 16:01:06 -0600

    worker method for administering data seeding

--- src/main/java/com/hftamayo/java/todo/TodoApplication.java
+++ src/main/java/com/hftamayo/java/todo/TodoApplication.java
@@ -1,13 +1,23 @@
 package com.hftamayo.java.todo;
 
+import com.hftamayo.java.todo.Services.UserSeeder;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TodoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner seedUser(UserSeeder userSeeder) {
+		return args -> {
+			userSeeder.seedDevelopment();
+		};
 	}
 
 }
