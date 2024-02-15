c372697263ee028cd549070a91b06aae1d27f1b9
hftamayo <hftamayo@gmail.com>
2024-02-15 15:45:48 -0600

    adding method for reload id numeration

--- src/main/java/com/hftamayo/java/todo/Repository/RolesRepository.java
+++ src/main/java/com/hftamayo/java/todo/Repository/RolesRepository.java
@@ -2,11 +2,19 @@ package com.hftamayo.java.todo.Repository;
 
 import com.hftamayo.java.todo.Model.Roles;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface RolesRepository extends JpaRepository<Roles, Long> {
+    @Modifying
+    @Query(value = "ALTER TABLE roles AUTO_INCREMENT = 1" , nativeQuery = true)
+    void resetAutoIncrement();
+
     @Override
     List<Roles> findAll();
     Optional<Roles> findByName(String name);
