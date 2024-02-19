85db60f821fb955b279e8c311ddaf7fbc33e2352
hftamayo <hftamayo@gmail.com>
2024-02-19 12:53:12 -0600

    removing call of UserSeeder class

--- src/main/java/com/hftamayo/java/todo/TodoApplication.java
+++ src/main/java/com/hftamayo/java/todo/TodoApplication.java
@@ -1,7 +1,5 @@
 package com.hftamayo.java.todo;
 
-import com.hftamayo.java.todo.Services.UserSeeder;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -11,12 +9,6 @@ public class TodoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TodoApplication.class, args);
-    }
-
-    @Bean
-    public CommandLineRunner seedUser(UserSeeder userSeeder) {
-        return args -> {
-        };
     }
 
 }
