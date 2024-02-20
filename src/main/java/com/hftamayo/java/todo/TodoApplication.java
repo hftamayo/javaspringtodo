94bd2169a0bbb761ce69048d623c63015178781d
hftamayo <hftamayo@gmail.com>
2024-02-20 12:32:18 -0600

    removing call to user seeder class

--- src/main/java/com/hftamayo/java/todo/TodoApplication.java
+++ src/main/java/com/hftamayo/java/todo/TodoApplication.java
@@ -2,7 +2,6 @@ package com.hftamayo.java.todo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TodoApplication {
