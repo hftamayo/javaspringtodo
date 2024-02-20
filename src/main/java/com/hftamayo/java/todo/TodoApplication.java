2a0fcee0fccc40aed29314f3e92695627a5e4f36
hftamayo <hftamayo@gmail.com>
2024-02-20 13:45:41 -0600

    calling userSeeder example

--- src/main/java/com/hftamayo/java/todo/TodoApplication.java
+++ src/main/java/com/hftamayo/java/todo/TodoApplication.java
@@ -1,13 +1,25 @@
 package com.hftamayo.java.todo;
 
+import com.hftamayo.java.todo.Services.UserSeeder;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TodoApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(TodoApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner seedData(UserSeeder userSeeder) {
+        return args -> {
+            System.out.println("Seeding data...");
+            userSeeder.onApplicationEvent(null);
+        };
     }
 
 }
