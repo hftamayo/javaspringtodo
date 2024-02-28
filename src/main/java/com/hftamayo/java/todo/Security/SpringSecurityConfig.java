bcea42f4cdd784dcba47bf01fb286bdd3c7ed995
hftamayo <hftamayo@gmail.com>
2024-02-28 12:24:57 -0600

    cleaning the class of unneeded imports

--- src/main/java/com/hftamayo/java/todo/Security/SpringSecurityConfig.java
+++ src/main/java/com/hftamayo/java/todo/Security/SpringSecurityConfig.java
@@ -1,14 +1,12 @@
 package com.hftamayo.java.todo.Security;
 
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
