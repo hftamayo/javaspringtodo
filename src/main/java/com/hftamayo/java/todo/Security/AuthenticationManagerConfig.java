873d78392db3ca731fb847527185794743589ed9
hftamayo <hftamayo@gmail.com>
2024-03-04 16:05:15 -0600

    expanding the source code of authManagerConfig class

--- src/main/java/com/hftamayo/java/todo/Security/AuthenticationManagerConfig.java
+++ src/main/java/com/hftamayo/java/todo/Security/AuthenticationManagerConfig.java
@@ -4,24 +4,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class AuthenticationManagerConfig {
 
+
     @Autowired
     private UserDetailsService userDetailsService;
 
     @Autowired
-    private PasswordEncoder passwordEncoder;
+    private AuthenticationManagerBuilder authenticationManagerBuilder;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public AuthenticationManager authenticationManagerBean() throws Exception {
-        return AuthenticationManagerBuilder
+        authenticationManagerBuilder
                 .userDetailsService(userDetailsService)
-                .passwordEncoder(passwordEncoder)
-                .and()
-                .build();
+                .passwordEncoder(passwordEncoder());
+        return authenticationManagerBuilder.build();
     }
 }
