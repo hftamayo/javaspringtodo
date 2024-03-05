a0b270a907ce402ba6741d3e96a45f64b8ca7a3a
hftamayo <hftamayo@gmail.com>
2024-03-05 13:39:23 -0600

    extending the use of customUserDetailsService for authentication management

--- src/main/java/com/hftamayo/java/todo/Services/CustomUserDetailsService.java
+++ src/main/java/com/hftamayo/java/todo/Services/CustomUserDetailsService.java
@@ -29,7 +29,7 @@ public class CustomUserDetailsService implements UserDetailsService {
                 .collect(Collectors.toSet());
 
         return new org.springframework.security.core.userdetails.User(
-                usernameOrEmail,
+                user.getEmail(),
                 user.getPassword(),
                 authorities
         );
