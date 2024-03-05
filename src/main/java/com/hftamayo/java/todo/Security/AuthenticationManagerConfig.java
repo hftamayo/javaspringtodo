e55e8990e3a24dc7a2b763878ad8d9452442e3f3
hftamayo <hftamayo@gmail.com>
2024-03-05 13:40:01 -0600

    using the customUserauthencationService for auth handshaking

--- src/main/java/com/hftamayo/java/todo/Security/AuthenticationManagerConfig.java
+++ src/main/java/com/hftamayo/java/todo/Security/AuthenticationManagerConfig.java
@@ -1,11 +1,11 @@
 package com.hftamayo.java.todo.Security;
 
+import com.hftamayo.java.todo.Services.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -14,26 +14,27 @@ public class AuthenticationManagerConfig {
 
 
     @Autowired
-    private final UserDetailsService userDetailsService;
+    private final CustomUserDetailsService customUserDetailsService;
 
     @Autowired
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
 
-    public AuthenticationManagerConfig(UserDetailsService userDetailsService,
+    public AuthenticationManagerConfig(CustomUserDetailsService customUserDetailsService,
                                        AuthenticationManagerBuilder authenticationManagerBuilder) {
-        this.userDetailsService = userDetailsService;
+        this.customUserDetailsService = customUserDetailsService;
         this.authenticationManagerBuilder = authenticationManagerBuilder;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        authenticationManagerBuilder
+                .userDetailsService(customUserDetailsService)
+                .passwordEncoder(passwordEncoder());
+        return authenticationManagerBuilder.build();
     }
 
     private PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        authenticationManagerBuilder
-                .userDetailsService(userDetailsService)
-                .passwordEncoder(passwordEncoder());
-        return authenticationManagerBuilder.build();
-    }
 }
