068f70c71aac0f86fc82d65eef77e0a794aa065e
hftamayo <hftamayo@gmail.com>
2024-03-22 12:45:15 -0600

    FilterConfig first stage

--- src/main/java/com/hftamayo/java/todo/security/FilterConfig.java
+++ src/main/java/com/hftamayo/java/todo/security/FilterConfig.java
@@ -1,23 +1,41 @@
 package com.hftamayo.java.todo.security;
 
-import com.hftamayo.java.todo.security.jwt.AuthenticationTokenFilter;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import com.hftamayo.java.todo.security.jwt.AuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-public class FilterConfig {
-    @Autowired
+@EnableWebSecurity
+@RequiredArgsConstructor
 
-    private AuthenticationTokenFilter jwtAuthenticationTokenFilter;
+public class FilterConfig {
+    private final AuthenticationFilter authenticationFilter;
+    private final AuthenticationProvider authenticationProvider;
 
     @Bean
-    public FilterRegistrationBean<AuthenticationTokenFilter> jwtFilterRegistrationBean() {
-        FilterRegistrationBean<AuthenticationTokenFilter> registrationBean = new FilterRegistrationBean<>();
-        registrationBean.setFilter(jwtAuthenticationTokenFilter);
-        registrationBean.setOrder(1); // set order here
-        return registrationBean;
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(csrf ->
+                        csrf
+                                .disable())
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/api/auth/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .sessionManagement(sessionManager ->
+                        sessionManager
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
 
 }
