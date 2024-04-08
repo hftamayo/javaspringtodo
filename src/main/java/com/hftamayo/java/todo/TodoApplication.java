74d3b14c93a1a833d6dafbcd07b08152011cf38b
Herbert Tamayo <hftamayo@gmail.com>
2024-04-08 13:21:24 -0600

    cleaning the main function

--- src/main/java/com/hftamayo/java/todo/TodoApplication.java
+++ src/main/java/com/hftamayo/java/todo/TodoApplication.java
@@ -1,10 +1,7 @@
 package com.hftamayo.java.todo;
 
-import com.hftamayo.java.todo.Services.UserSeeder;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -15,5 +12,4 @@ public class TodoApplication {
 
         SpringApplication.run(TodoApplication.class, args);
     }
-
 }
