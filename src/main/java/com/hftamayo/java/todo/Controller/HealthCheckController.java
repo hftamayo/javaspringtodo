cc0f17c1d7cfcff78ade9603878e00b8e4304556
Herbert Tamayo <hftamayo@gmail.com>
2024-04-10 11:18:14 -0600

    controller for a basic health check

---
+++ src/main/java/com/hftamayo/java/todo/Controller/HealthCheckController.java
@@ -0,0 +1,42 @@
+package com.hftamayo.java.todo.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLException;
+import java.util.Date;
+
+@RestController
+@RequestMapping("/api/health")
+public class HealthCheckController {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public HealthCheckController(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @GetMapping("/app")
+    public ResponseEntity<String> checkAppHealth() {
+        return new ResponseEntity<>("HealthCheck: Application is up and running. " +
+                "Timestamp: " + new Date().toString(), HttpStatus.OK);
+    }
+
+    @GetMapping("/db")
+    public ResponseEntity<String> checkDbHealth() {
+        try {
+            this.jdbcTemplate.getDataSource().getConnection().close();
+            return new ResponseEntity<>("HealthCheck: The connection to the data layer is up and running. " +
+                    "Timestamp: " + new Date().toString(), HttpStatus.OK);
+        } catch (SQLException ex) {
+            return new ResponseEntity<>("HealthCheck: The connection to the data layer is down. " +
+                    "Timestamp: " + new Date().toString() + ". Error: " +
+                    ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
