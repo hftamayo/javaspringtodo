7c393b816e3ab6efdad392b2ad98c648c7b09064
Herbert Tamayo <hftamayo@gmail.com>
2024-04-11 12:40:51 -0600

    adjusting User class to UserDetails interface

--- src/main/java/com/hftamayo/java/todo/Model/User.java
+++ src/main/java/com/hftamayo/java/todo/Model/User.java
@@ -5,8 +5,11 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.Set;
 
 @Data
@@ -16,7 +19,7 @@ import java.util.Set;
 
 @Entity
 @Table(schema = "users")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -38,18 +41,30 @@ public class User {
     @Builder.Default
     private boolean isAdmin = false;
 
+    @Column(nullable = false, name= "isAccountNonExpired")
+    @Builder.Default
+    private boolean isAccountNonExpired = true;
+
+    @Column(nullable = false, name= "isAccountNonLocked")
+    @Builder.Default
+    private boolean isAccountNonLocked = true;
+
+    @Column(nullable = false, name= "isCredentialsNonExpired")
+    @Builder.Default
+    private boolean isCredentialsNonExpired = true;
+
     @Column(nullable = false, name = "user_status")
     @Builder.Default
     private boolean status = true;
 
     @Column
     @CreatedDate
-    @JsonFormat(pattern = "dd-MM-yyyy HH:mm", timezone = "America/El_Salvador")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm", timezone = "America/El_Salvador")
     private LocalDateTime dateAdded;
 
     @Column
     @LastModifiedDate
-    @JsonFormat(pattern = "dd-MM-yyyy HH:mm", timezone = "America/El_Salvador")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm", timezone = "America/El_Salvador")
     private LocalDateTime dateUpdated;
 
     @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
@@ -60,6 +75,43 @@ public class User {
             inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
     )
     private Set<Roles> roles;
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return this.isAccountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return this.isAccountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return this.isCredentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.status;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles;
+    }
+
+
 
 //    @PrePersist
 //    protected void onCreate() {
