29b84131410f077681ea7324d7af02388050e08e
Herbert Tamayo <hftamayo@gmail.com>
2024-04-11 12:08:08 -0600

    adding logger to debug login workflow

--- src/main/java/com/hftamayo/java/todo/Controller/AuthController.java
+++ src/main/java/com/hftamayo/java/todo/Controller/AuthController.java
@@ -7,6 +7,8 @@ import com.hftamayo.java.todo.Model.User;
 import com.hftamayo.java.todo.Services.AuthService;
 import com.hftamayo.java.todo.Services.UserService;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,17 +17,26 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
+    private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
     private AuthService authService;
     private UserService userService;
 
     @PostMapping("/login")
     public ResponseEntity<String> authenticate(@RequestBody LoginDto loginDto) {
-        TokenResponseDto tokenResponseDto = authService.login(loginDto);
+        logger.info("Login attempt: " + loginDto.getEmail() + " " + loginDto.getPassword());
+        try {
+            TokenResponseDto tokenResponseDto = authService.login(loginDto);
 
-        System.out.println("token: " + tokenResponseDto.getAccessToken() + " type: " +
-                tokenResponseDto.getTokenType() + " expires in: " + tokenResponseDto.getExpiresIn());
+            logger.info("Login successful, welcome: " + loginDto.getEmail() + "!");
 
-        return ResponseEntity.ok("Login successful, welcome: " + loginDto.getEmail() + "!");
+            System.out.println("token: " + tokenResponseDto.getAccessToken() + " type: " +
+                    tokenResponseDto.getTokenType() + " expires in: " + tokenResponseDto.getExpiresIn());
+
+            return ResponseEntity.ok("Login successful, welcome: " + loginDto.getEmail() + "!");
+        } catch (Exception e) {
+            logger.error("Error in login: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
+        }
     }
 
     @PostMapping("/register")
