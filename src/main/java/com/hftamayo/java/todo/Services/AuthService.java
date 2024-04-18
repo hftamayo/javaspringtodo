d6f77a625b8e2ab80e9ecf6f53933370d74a49ea
Herbert Tamayo <hftamayo@gmail.com>
2024-04-18 12:16:47 -0600

    adding a method for logout workflow

--- src/main/java/com/hftamayo/java/todo/Services/AuthService.java
+++ src/main/java/com/hftamayo/java/todo/Services/AuthService.java
@@ -4,9 +4,13 @@ import com.hftamayo.java.todo.Dto.LoginDto;
 import com.hftamayo.java.todo.Dto.TokenResponseDto;
 import com.hftamayo.java.todo.Dto.UserResponseDto;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 public interface AuthService {
 
     TokenResponseDto login(LoginDto loginRequest);
+
+    void logout(HttpServletRequest request);
 
     void invalidateToken();
 
