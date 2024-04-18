8f07b3b4bf165a22ee9ef5dd678a92a1836a466b
Herbert Tamayo <hftamayo@gmail.com>
2024-04-18 11:14:15 -0600

    using request mapping annotation

--- src/main/java/com/hftamayo/java/todo/Controller/TaskController.java
+++ src/main/java/com/hftamayo/java/todo/Controller/TaskController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/user")
 public class TaskController {
     private final TaskService taskService;
 
