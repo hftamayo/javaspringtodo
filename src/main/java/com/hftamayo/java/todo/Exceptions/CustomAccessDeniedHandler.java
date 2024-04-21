38b62b8ecf8bc7716be8b01ceba3179196150d95
Herbert Tamayo <hftamayo@gmail.com>
2024-04-20 19:14:56 -0600

    adding component annotation

--- src/main/java/com/hftamayo/java/todo/Exceptions/CustomAccessDeniedHandler.java
+++ src/main/java/com/hftamayo/java/todo/Exceptions/CustomAccessDeniedHandler.java
@@ -4,9 +4,11 @@ import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+@Component
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
 
     @Override
