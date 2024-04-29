97d6b0ee2a43e64cab1a0391f26f2fba852a3028
Herbert Tamayo <hftamayo@gmail.com>
2024-04-29 11:25:43 -0600

    adding a class in charge of the management of the context of authenticated user and its requests

---
+++ src/main/java/com/hftamayo/java/todo/security/managers/CustomAccessDecisionManager.java
@@ -0,0 +1,40 @@
+package com.hftamayo.java.todo.security.managers;
+
+import org.springframework.security.access.AccessDecisionManager;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.access.ConfigAttribute;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+
+public class CustomAccessDecisionManager implements AccessDecisionManager {
+
+    @Override
+    public void decide(Authentication authentication, Object object, Collection<ConfigAttribute> configAttributes)
+            throws AccessDeniedException {
+        String requiredRole = configAttributes.stream()
+                .map(ConfigAttribute::getAttribute)
+                .findFirst()
+                .orElseThrow(() -> new AccessDeniedException("No required roles found"));
+
+        String userRole = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .findFirst()
+                .orElseThrow(() -> new AccessDeniedException("No roles found for user"));
+
+        if (!requiredRole.equals(userRole)) {
+            throw new AccessDeniedException("Access Denied: Required role: " + requiredRole + ", but given role: " + userRole);
+        }
+    }
+
+    @Override
+    public boolean supports(ConfigAttribute attribute) {
+        return true;
+    }
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return true;
+    }
+}
