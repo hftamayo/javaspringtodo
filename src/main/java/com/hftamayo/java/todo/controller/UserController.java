9d8e0f8ee0f521f6fe4f4e4dd64d5027eff1a4b5
Herbert Tamayo <hftamayo@gmail.com>
2024-04-29 10:31:02 -0600

    improving endpoint name related to user activation

--- src/main/java/com/hftamayo/java/todo/controller/UserController.java
+++ src/main/java/com/hftamayo/java/todo/controller/UserController.java
@@ -12,7 +12,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -121,7 +120,7 @@ public class UserController {
         }
     }
 
-    @PatchMapping(value = "/users/updatestatusandrole/{userId}")
+    @PatchMapping(value = "/users/activateuser/{userId}")
     public ResponseEntity<User> updateUserStatusAndRole(@PathVariable long userId,
                                                         @RequestBody Map<String, Object> updates) {
         try {
