e79532bd8761520963f10ac0fbb87783ef390f99
Herbert Tamayo <hftamayo@gmail.com>
2024-04-29 11:35:09 -0600

    coding the manager in charge to get details during access requests based on the active session

--- src/main/java/com/hftamayo/java/todo/security/managers/CustomAccessDecisionManager.java
+++ src/main/java/com/hftamayo/java/todo/security/managers/CustomAccessDecisionManager.java
@@ -5,9 +5,11 @@ import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.ConfigAttribute;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
 
 import java.util.Collection;
 
+@Component
 public class CustomAccessDecisionManager implements AccessDecisionManager {
 
     @Override
