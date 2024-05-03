2251dc7ba642199ff639ce19e967a5ff017303c0
Herbert Tamayo <hftamayo@gmail.com>
2024-05-03 12:11:14 -0600

    adding a condition to bypass filtering if the requests comes from register or login workflows

--- src/main/java/com/hftamayo/java/todo/security/managers/CustomAccessDecisionManager.java
+++ src/main/java/com/hftamayo/java/todo/security/managers/CustomAccessDecisionManager.java
@@ -5,9 +5,13 @@ import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.ConfigAttribute;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.FilterInvocation;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Component
 public class CustomAccessDecisionManager implements AccessDecisionManager {
@@ -15,24 +19,28 @@ public class CustomAccessDecisionManager implements AccessDecisionManager {
     @Override
     public void decide(Authentication authentication, Object object, Collection<ConfigAttribute> configAttributes)
             throws AccessDeniedException {
-        if (configAttributes == null || configAttributes.isEmpty()) {
-            throw new AccessDeniedException("Access Denied: No required roles found");
+        FilterInvocation filterInvocation = (FilterInvocation) object;
+        String url = filterInvocation.getRequestUrl();
+
+        if (url.startsWith("/api/auth/login") || url.startsWith("/api/auth/register")) {
+            // Allow the request to proceed if it's for the register or login workflows
+            return;
         }
 
-        String requiredRole = configAttributes.stream()
+        Set<String> requiredRoles = configAttributes.stream()
                 .map(ConfigAttribute::getAttribute)
-                .findFirst()
-                .orElseThrow(() -> new AccessDeniedException("No required roles found"));
+                .collect(Collectors.toSet());
 
-        String userRole = authentication.getAuthorities().stream()
+        Set<String> userRoles = authentication.getAuthorities().stream()
                 .map(GrantedAuthority::getAuthority)
-                .findFirst()
-                .orElseThrow(() -> new AccessDeniedException("No roles found for user"));
+                .collect(Collectors.toSet());
 
-        if (!requiredRole.equals(userRole)) {
-            throw new AccessDeniedException("Access Denied: Required role: " + requiredRole + ", but given role: " + userRole);
+        // Check if userRoles contains any of the requiredRoles
+        if (Collections.disjoint(userRoles, requiredRoles)) {
+            throw new AccessDeniedException("Access Denied: Required roles: " + requiredRoles + ", but given roles: " + userRoles);
         }
     }
+
 
     @Override
     public boolean supports(ConfigAttribute attribute) {
