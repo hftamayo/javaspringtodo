15673d7477d45e3c60fce49de83f75ba964604c8
Herbert Tamayo <hftamayo@gmail.com>
2024-05-23 10:19:11 -0600

    formatting code

--- src/main/java/com/hftamayo/java/todo/controller/RolesController.java
+++ src/main/java/com/hftamayo/java/todo/controller/RolesController.java
@@ -20,43 +20,43 @@ public class RolesController {
 
     @GetMapping(value = "/allroles")
     @ResponseStatus(HttpStatus.OK)
-    public List<Roles> getRoles(){
+    public List<Roles> getRoles() {
         return rolesService.getRoles();
     }
 
     @GetMapping(value = "/getrolebyname/{roleName}")
     @ResponseStatus(HttpStatus.OK)
-    public Optional<Roles> getRoleByName(@PathVariable String roleName){
+    public Optional<Roles> getRoleByName(@PathVariable String roleName) {
         return rolesService.getRoleByEnum(roleName);
     }
 
     @PostMapping(value = "/saverole")
     @ResponseStatus(HttpStatus.CREATED)
-    public Roles saveRole(@RequestBody Roles role){
+    public Roles saveRole(@RequestBody Roles role) {
         return rolesService.saveRole(role);
     }
 
-    @PutMapping(value="/updaterole/{roleId}")
-    public ResponseEntity<Roles> updateRole(@PathVariable long roleId, @RequestBody Roles role){
-        try{
+    @PutMapping(value = "/updaterole/{roleId}")
+    public ResponseEntity<Roles> updateRole(@PathVariable long roleId, @RequestBody Roles role) {
+        try {
             Roles updatedRole = rolesService.updateRole(roleId, role);
             return new ResponseEntity<>(updatedRole, HttpStatus.OK);
-        }catch (EntityNotFoundException enf){
+        } catch (EntityNotFoundException enf) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }catch (Exception e){
+        } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
-    @DeleteMapping(value="/deleterole/{roleId}")
+    @DeleteMapping(value = "/deleterole/{roleId}")
     @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity<?> deleteRole(@PathVariable long roleId){
-        try{
+    public ResponseEntity<?> deleteRole(@PathVariable long roleId) {
+        try {
             rolesService.deleteRole(roleId);
             return new ResponseEntity<>(HttpStatus.OK);
-        }catch (EntityNotFoundException enf){
+        } catch (EntityNotFoundException enf) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }catch (Exception e){
+        } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
