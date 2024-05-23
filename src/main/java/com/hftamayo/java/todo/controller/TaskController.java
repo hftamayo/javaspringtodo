4a94cbd4ffcf4a1815bac643ab0a428537504b33
Herbert Tamayo <hftamayo@gmail.com>
2024-05-23 10:36:52 -0600

    adding annotation and improving code formatting

--- src/main/java/com/hftamayo/java/todo/controller/TaskController.java
+++ src/main/java/com/hftamayo/java/todo/controller/TaskController.java
@@ -3,6 +3,7 @@ package com.hftamayo.java.todo.controller;
 import com.hftamayo.java.todo.model.Task;
 import com.hftamayo.java.todo.services.TaskService;
 import jakarta.persistence.EntityNotFoundException;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,69 +11,64 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@AllArgsConstructor
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api/todos")
 public class TaskController {
     private final TaskService taskService;
 
-    @Autowired
-    public TaskController(TaskService taskService){
-        this.taskService = taskService;
-    }
-
-
-    @GetMapping(value = "/todos/alltasks")
+    @GetMapping(value = "/alltasks")
     @ResponseStatus(HttpStatus.OK)
-    public List<Task> getTasks(){
+    public List<Task> getTasks() {
         return taskService.getTasks();
     }
 
-    @GetMapping(value = "/todos/gettaskbyid/{taskId}")
+    @GetMapping(value = "/gettaskbyid/{taskId}")
     @ResponseStatus(HttpStatus.OK)
-    public Task getTask(@PathVariable long taskId){
+    public Task getTask(@PathVariable long taskId) {
         return taskService.getTaskById(taskId);
     }
 
-    @GetMapping(value = "/todos/gettaskbytitle/{taskTitle}")
+    @GetMapping(value = "/gettaskbytitle/{taskTitle}")
     @ResponseStatus(HttpStatus.OK)
-    public Task getTaskByTitle(@PathVariable String taskTitle){
+    public Task getTaskByTitle(@PathVariable String taskTitle) {
         return taskService.getTaskByTitle(taskTitle);
     }
 
-    @GetMapping(value = "/todos/gettasksbystatus/{status}")
+    @GetMapping(value = "/gettasksbystatus/{status}")
     @ResponseStatus(HttpStatus.OK)
-    public List<Task> getTasksByStatus(@PathVariable boolean status){
+    public List<Task> getTasksByStatus(@PathVariable boolean status) {
         return taskService.getAllTasksByStatus(status);
     }
 
-    @GetMapping(value = "/todos/counttasksbystatus/{status}")
+    @GetMapping(value = "/counttasksbystatus/{status}")
     @ResponseStatus(HttpStatus.OK)
-    public long countTasksByStatus(@PathVariable boolean status){
+    public long countTasksByStatus(@PathVariable boolean status) {
         return taskService.countAllTaskByStatus(status);
     }
 
 
-    @PostMapping(value = "/todos/savetask")
+    @PostMapping(value = "/savetask")
     @ResponseStatus(HttpStatus.CREATED)
-    public Task saveTask(@RequestBody Task task){
+    public Task saveTask(@RequestBody Task task) {
         return taskService.saveTask(task);
     }
 
-    @PutMapping(value="/todos/updatetask/{taskId}")
-    public ResponseEntity<Task> updateTask(@PathVariable long taskId, @RequestBody Task task){
+    @PutMapping(value = "/updatetask/{taskId}")
+    public ResponseEntity<Task> updateTask(@PathVariable long taskId, @RequestBody Task task) {
         Task updatedTask = taskService.updateTask(taskId, task);
         return new ResponseEntity<>(updatedTask, HttpStatus.OK);
     }
 
-    @DeleteMapping(value="/todos/deletetask/{taskId}")
+    @DeleteMapping(value = "/deletetask/{taskId}")
     @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity<?> deleteTask(@PathVariable long taskId){
+    public ResponseEntity<?> deleteTask(@PathVariable long taskId) {
         try {
             taskService.deleteTask(taskId);
             return ResponseEntity.ok().build();
-        }catch (EntityNotFoundException enf){
+        } catch (EntityNotFoundException enf) {
             return new ResponseEntity<>(enf.getMessage(), HttpStatus.NOT_FOUND);
-        } catch (Exception e){
+        } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
