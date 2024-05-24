8f26ad24b3be6b3556860fb7bd77c8cbf618bd95
Herbert Tamayo <hftamayo@gmail.com>
2024-05-24 10:31:36 -0600

    adding a method to search records by ID

--- src/main/java/com/hftamayo/java/todo/repository/TaskRepository.java
+++ src/main/java/com/hftamayo/java/todo/repository/TaskRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
@@ -12,8 +13,12 @@ public interface TaskRepository extends JpaRepository<Task, Long> {
     @Override
     List<Task> findAll();
     List <Task> findAllByStatus(boolean isActive);
+
+    Optional<Task> findById(long taskId);
+
     Task findByTitle(String title);
     long countAllByStatus(boolean isActive);
+
 
     //for next release
     // List<Task> findAllByOwner(long taskOwner);
