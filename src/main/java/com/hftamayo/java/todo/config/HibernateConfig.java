420d27a637cd15ff3aba81b142998c606c0ea7d0
Herbert Tamayo <hftamayo@gmail.com>
2024-06-12 09:58:35 -0600

    config class for Hibernate operations

---
+++ src/main/java/com/hftamayo/java/todo/config/HibernateConfig.java
@@ -0,0 +1,36 @@
+package com.hftamayo.java.todo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+public class HibernateConfig {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        sessionFactory.setPackagesToScan("com.hftamayo.java.todo.model");
+        sessionFactory.setHibernateProperties(hibernateProperties());
+
+        return sessionFactory;
+    }
+
+    @Bean
+    public Properties hibernateProperties() {
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "update");
+        hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+
+        return hibernateProperties;
+    }
+
+}
