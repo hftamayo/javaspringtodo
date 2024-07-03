07c20d1cea08fb802a857643bd2802b91c33489c
Herbert Tamayo <hftamayo@gmail.com>
2024-07-03 11:37:04 -0600

    adjusting methods acording to userDao class

--- src/main/java/com/hftamayo/java/todo/services/UserService.java
+++ src/main/java/com/hftamayo/java/todo/services/UserService.java
@@ -2,29 +2,38 @@ package com.hftamayo.java.todo.services;
 
 import com.hftamayo.java.todo.dto.RegisterUserResponseDto;
 import com.hftamayo.java.todo.model.User;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface UserService {
     List<User> getUsers();
-    List<User> getAllUsersByStatus(boolean isActive);
+
     Optional<User> getUserById(long userId);
-    Optional<User> getUserByUsername(String username);
-    Optional<User> getUserByEmail(String email);
-    User getUserByUsernameAndPassword(String username, String password);
-    User getUserByEmailAndPassword(String email, String password);
-    long countAllUserByUsername(String username);
-    long countAllUserByEmail(String email);
+
+    List<User> getUsersByStatus(boolean isActive);
+
+    Optional<User> getUserByEmail(String userEmail);
+
+    Optional<User> getUserByName(String userName);
+
+    Optional<User> getUserByNameAndPassword(String userName, String userPassword);
+
+    Optional<User> getUserByEmailAndPassword(String userEmail, String userPassword);
+
+    long countAllByCriteria(String criteria, String value);
 
     User saveUser(User newUser);
-    RegisterUserResponseDto userToDto(User user);
+
     User updateUser(long userId, User updatedUser);
-    User updateUserStatus(long userId, boolean status);
+
     void deleteUser(long userId);
 
+    User updateUserStatus(long userId, boolean status);
+
     User updateUserStatusAndRole(long userId, boolean status, String roleEnum);
+
+    RegisterUserResponseDto userToDto(User user);
 
 
 }
