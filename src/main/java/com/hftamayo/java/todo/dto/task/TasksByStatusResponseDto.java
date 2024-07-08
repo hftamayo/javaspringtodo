89088aad3a3ca04f282e5683cb57c3a0613eb154
Herbert Tamayo <hftamayo@gmail.com>
2024-07-08 11:45:30 -0600

    using lombok annotations to simplify dto stuct

--- src/main/java/com/hftamayo/java/todo/dto/task/TasksByStatusResponseDto.java
+++ src/main/java/com/hftamayo/java/todo/dto/task/TasksByStatusResponseDto.java
@@ -1,33 +1,20 @@
 package com.hftamayo.java.todo.dto.task;
 
 import com.hftamayo.java.todo.model.Task;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class TasksByStatusResponseDto {
     private List<Task> tasks;
     private int count;
 
-    public TasksByStatusResponseDto(List<Task> tasks) {
-        this.tasks = tasks;
-        this.count = tasks != null ? tasks.size() : 0;
-    }
 
-    // Getters and Setters
-    public List<Task> getTasks() {
-        return tasks;
-    }
-
-    public void setTasks(List<Task> tasks) {
-        this.tasks = tasks;
-        this.count = tasks != null ? tasks.size() : 0;
-    }
-
-    public int getCount() {
-        return count;
-    }
-
-    public void setCount(int count) {
-        this.count = count;
-    }
 }
