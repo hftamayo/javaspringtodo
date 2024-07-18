67f0b5a3be19f42d75e128b7c2703167eaf2a1d6
Herbert Tamayo <hftamayo@gmail.com>
2024-07-18 15:27:14 -0600

    updating taskService class

--- src/main/java/com/hftamayo/java/todo/services/TaskService.java
+++ src/main/java/com/hftamayo/java/todo/services/TaskService.java
@@ -1,19 +1,18 @@
 package com.hftamayo.java.todo.services;
 
-import com.hftamayo.java.todo.dto.task.TasksByStatusResponseDto;
 import com.hftamayo.java.todo.model.Task;
+import com.hftamayo.java.todo.dto.task.TaskResponseDto;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface TaskService {
-    List<Task> getTasks();
-    Optional<Task> getTaskById(long taskId);
-    List<Task> getAllTasksByStatus(boolean isActive);
-    Optional<Task> getTaskByTitle(String title);
-    long countAllTaskByStatus(boolean isActive);
-    TasksByStatusResponseDto getTasksByStatusAndSize(boolean isActive);
+    List<TaskResponseDto> getTasks();
+    Optional<TaskResponseDto> getTaskById(long taskId);
+    Optional<List<TaskResponseDto>> getTaskByCriteria(String criteria, String value);
+    Optional<List<TaskResponseDto>> getTaskByCriterias(String criteria, String value, String criteria2, String value2);
 
+    Optional<Task> getTaskByTitle(String title);
     Task saveTask(Task newTask);
     Task updateTask(long taskId, Task updatedTask);
     void deleteTask(long taskId);
