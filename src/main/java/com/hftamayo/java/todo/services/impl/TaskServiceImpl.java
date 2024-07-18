d7ae0bd605e5c37c48c151dcd208486c1ccb3c73
Herbert Tamayo <hftamayo@gmail.com>
2024-07-18 15:47:25 -0600

    adding taskToDto methods

--- src/main/java/com/hftamayo/java/todo/services/impl/TaskServiceImpl.java
+++ src/main/java/com/hftamayo/java/todo/services/impl/TaskServiceImpl.java
@@ -1,6 +1,7 @@
 package com.hftamayo.java.todo.services.impl;
 
 import com.hftamayo.java.todo.dao.TaskDao;
+import com.hftamayo.java.todo.dto.task.TaskResponseDto;
 import com.hftamayo.java.todo.dto.task.TasksByStatusResponseDto;
 import com.hftamayo.java.todo.exceptions.EntityAlreadyExistsException;
 import com.hftamayo.java.todo.model.Task;
@@ -13,6 +14,7 @@ import jakarta.persistence.EntityNotFoundException;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -20,28 +22,28 @@ public class TaskServiceImpl implements TaskService {
     @Autowired
     private final TaskDao taskDao;
 
-    public List<Task> getTasks(){
+    public List<TaskResponseDto> getTasks() {
         return taskDao.getTasks();
     }
 
-    public Optional<Task> getTaskById(long taskId){
+    public Optional<Task> getTaskById(long taskId) {
         return taskDao.getTaskById(taskId);
     }
 
-    public List<Task> getAllTasksByStatus(boolean taskStatus){
+    public List<Task> getAllTasksByStatus(boolean taskStatus) {
         return taskDao.getTasksByStatus(taskStatus);
     }
 
-    public Optional<Task> getTaskByTitle(String taskTitle){
+    public Optional<Task> getTaskByTitle(String taskTitle) {
         return taskDao.getTaskByTitle(taskTitle);
     }
 
-    public long countAllTaskByStatus(boolean taskStatus){
+    public long countAllTaskByStatus(boolean taskStatus) {
         List<Task> tasks = taskDao.getTasksByStatus(taskStatus);
         return tasks.size();
     }
 
-    public TasksByStatusResponseDto getTasksByStatusAndSize(boolean taskStatus){
+    public TasksByStatusResponseDto getTasksByStatusAndSize(boolean taskStatus) {
         List<Task> tasks = taskDao.getTasksByStatus(taskStatus);
         int count = tasks.size();
         return new TasksByStatusResponseDto(tasks, count);
@@ -49,9 +51,9 @@ public class TaskServiceImpl implements TaskService {
 
     @Transactional
     @Override
-    public Task saveTask(Task task){
+    public Task saveTask(Task task) {
         Optional<Task> requestedTask = getTaskByTitle(task.getTitle());
-        if(requestedTask.isPresent()){
+        if (requestedTask.isPresent()) {
             throw new EntityAlreadyExistsException("Title already exists");
         }
         return taskDao.saveTask(task);
@@ -61,7 +63,7 @@ public class TaskServiceImpl implements TaskService {
     @Override
     public Task updateTask(long taskId, Task task) {
         Optional<Task> requestedTask = getTaskById(taskId);
-        if(requestedTask.isPresent()){
+        if (requestedTask.isPresent()) {
             Task updateTask = requestedTask.get();
             updateTask.setTitle(task.getTitle());
             updateTask.setDescription(task.getDescription());
@@ -76,13 +78,31 @@ public class TaskServiceImpl implements TaskService {
     @Override
     public void deleteTask(long taskId) {
         Optional<Task> requestedTask = getTaskById(taskId);
-        if(requestedTask.isPresent()){
+        if (requestedTask.isPresent()) {
             taskDao.deleteTask(requestedTask.get().getId());
         } else {
             throw new EntityNotFoundException("Task does not exist");
         }
     }
-    
+
+    @Override
+    public TaskResponseDto taskToDto(Task task) {
+        String owner = task.getUser().getUsername();
+
+        return new TaskResponseDto(
+                task.getId(),
+                task.getTitle(),
+                task.getDescription(),
+                task.isStatus(),
+                task.getDateAdded().toString(),
+                owner
+        );
+    }
+
+    private List<TaskResponseDto> taskListToDto(List<Task> tasks) {
+        return tasks.stream().map(this::taskToDto).collect(Collectors.toList());
+    }
+
 }
 
 
