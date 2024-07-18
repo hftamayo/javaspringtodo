dbf4eb5e10bbf904c95e623e009c1e928e11a270
Herbert Tamayo <hftamayo@gmail.com>
2024-07-18 15:25:12 -0600

    adding TaskResponseDto class

---
+++ src/main/java/com/hftamayo/java/todo/dto/task/TaskResponseDto.java
@@ -0,0 +1,20 @@
+package com.hftamayo.java.todo.dto.task;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class TaskResponseDto {
+    private Long id;
+    private String title;
+    private String description;
+    private String status;
+    private String dateAdded;
+    private String owner;
+}
