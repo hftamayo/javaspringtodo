09ceeac58d6f5dbe1064cfcddc7ed66b25538cdb
Herbert Tamayo <hftamayo@gmail.com>
2024-07-25 15:45:05 -0600

    adding EnableSec annotation and Lazy to UserService to avoid circularDep

--- src/main/java/com/hftamayo/java/todo/security/SpringSecurityConfig.java
+++ src/main/java/com/hftamayo/java/todo/security/SpringSecurityConfig.java
@@ -7,10 +7,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -18,12 +20,13 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
 @RequiredArgsConstructor
 public class SpringSecurityConfig {
 
     private static final Logger logger = LoggerFactory.getLogger(SpringSecurityConfig.class);
 
-    private final UserService userService;
+    private final @Lazy UserService userService;
 
     @Bean
     public JwtConfig jwtConfig() {
