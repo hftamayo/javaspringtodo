fb32bbe4989622d8e03d42f9046a721083267899
Herbert Tamayo <hftamayo@gmail.com>
2024-08-02 21:39:39 -0600

    using a config class to manage the access to the authenticated user

---
+++ src/main/java/com/hftamayo/java/todo/security/UserDetailsServiceConfig.java
@@ -0,0 +1,23 @@
+package com.hftamayo.java.todo.security;
+
+import com.hftamayo.java.todo.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+@RequiredArgsConstructor
+public class UserDetailsServiceConfig {
+
+    private final @Lazy UserService userService;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return userEmail -> (UserDetails) userService.getUserByEmail(userEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("Invalid Credentials: Username or Password not found"));
+    }
+}
