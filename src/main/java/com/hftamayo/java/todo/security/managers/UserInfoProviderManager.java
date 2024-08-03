44280fd9f158d89d3b35646a96b418b8c39e9214
Herbert Tamayo <hftamayo@gmail.com>
2024-08-02 21:40:19 -0600

    introducing a pipeline of a class in charge just to get the detauls of the authenticated user

---
+++ src/main/java/com/hftamayo/java/todo/security/managers/UserInfoProviderManager.java
@@ -0,0 +1,18 @@
+package com.hftamayo.java.todo.security.managers;
+
+import com.hftamayo.java.todo.security.interfaces.UserInfoProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserInfoProviderManager implements UserInfoProvider {
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    public UserDetails getUserDetails(String username) {
+        return userDetailsService.loadUserByUsername(username);
+    }
+}
