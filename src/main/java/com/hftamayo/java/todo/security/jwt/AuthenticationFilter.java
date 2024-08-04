4ee9df8519011443e4098a5e5bd651fb4ec2e2f4
Herbert Tamayo <hftamayo@gmail.com>
2024-08-04 07:39:02 -0600

    adding Lazy annotation to UserInfoProvier and UserDetailsService calls

--- src/main/java/com/hftamayo/java/todo/security/jwt/AuthenticationFilter.java
+++ src/main/java/com/hftamayo/java/todo/security/jwt/AuthenticationFilter.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -22,7 +23,7 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class AuthenticationFilter extends OncePerRequestFilter {
 
-    private final UserInfoProvider userInfoProvider;
+    private final @Lazy UserInfoProvider userInfoProvider;
     private final CustomTokenProvider customTokenProvider;
 
     private static final Logger logger = LoggerFactory.getLogger(AuthenticationFilter.class);
--- src/main/java/com/hftamayo/java/todo/security/managers/UserInfoProviderManager.java
+++ src/main/java/com/hftamayo/java/todo/security/managers/UserInfoProviderManager.java
@@ -2,6 +2,7 @@ package com.hftamayo.java.todo.security.managers;
 
 import com.hftamayo.java.todo.security.interfaces.UserInfoProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
@@ -9,7 +10,7 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class UserInfoProviderManager implements UserInfoProvider {
-    private final UserDetailsService userDetailsService;
+    private final @Lazy UserDetailsService userDetailsService;
 
     @Override
     public UserDetails getUserDetails(String username) {
