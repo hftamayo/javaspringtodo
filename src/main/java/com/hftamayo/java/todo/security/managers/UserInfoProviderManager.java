c9f24e88d22ee042351da97c7bdff32b1dceab5c
Herbert Tamayo <hftamayo@gmail.com>
2024-08-08 13:35:15 -0600

    removing Lazy annotation from userDetailsService

--- src/main/java/com/hftamayo/java/todo/security/managers/UserInfoProviderManager.java
+++ src/main/java/com/hftamayo/java/todo/security/managers/UserInfoProviderManager.java
@@ -2,7 +2,6 @@ package com.hftamayo.java.todo.security.managers;
 
 import com.hftamayo.java.todo.security.interfaces.UserInfoProvider;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
@@ -10,7 +9,7 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class UserInfoProviderManager implements UserInfoProvider {
-    private final @Lazy UserDetailsService userDetailsService;
+    private final UserDetailsService userDetailsService;
 
     @Override
     public UserDetails getUserDetails(String username) {
