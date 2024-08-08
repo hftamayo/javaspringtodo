5444d7dde5a4968d4ed1b49c162a615fefc9b12c
Herbert Tamayo <hftamayo@gmail.com>
2024-08-08 13:58:09 -0600

    removing unneeded import

--- src/main/java/com/hftamayo/java/todo/services/impl/UserServiceImpl.java
+++ src/main/java/com/hftamayo/java/todo/services/impl/UserServiceImpl.java
@@ -8,7 +8,6 @@ import com.hftamayo.java.todo.model.User;
 import com.hftamayo.java.todo.services.UserService;
 import com.hftamayo.java.todo.exceptions.EntityAlreadyExistsException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
