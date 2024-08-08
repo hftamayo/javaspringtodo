2d2427dde9ec73cfa7149a8bf823374970c63de0
Herbert Tamayo <hftamayo@gmail.com>
2024-08-08 13:57:16 -0600

    using autowired in the springSecConfig constructor

--- src/main/java/com/hftamayo/java/todo/security/SpringSecurityConfig.java
+++ src/main/java/com/hftamayo/java/todo/security/SpringSecurityConfig.java
@@ -5,6 +5,7 @@ import com.hftamayo.java.todo.security.managers.UserInfoProviderManager;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
