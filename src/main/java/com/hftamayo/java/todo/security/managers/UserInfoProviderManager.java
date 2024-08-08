5cc41f50d72ae5fc241b02f76f81e073316461ee
Herbert Tamayo <hftamayo@gmail.com>
2024-08-08 14:20:02 -0600

    refactoring the method getUserDetails based on userService

--- src/main/java/com/hftamayo/java/todo/security/managers/UserInfoProviderManager.java
+++ src/main/java/com/hftamayo/java/todo/security/managers/UserInfoProviderManager.java
@@ -1,18 +1,23 @@
 package com.hftamayo.java.todo.security.managers;
 
 import com.hftamayo.java.todo.security.interfaces.UserInfoProvider;
+import com.hftamayo.java.todo.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class UserInfoProviderManager implements UserInfoProvider {
-    private final UserDetailsService userDetailsService;
+    private final UserService userService;
 
     @Override
     public UserDetails getUserDetails(String username) {
-        return userDetailsService.loadUserByUsername(username);
+        return userService.getUserByEmail(username)
+                .orElseThrow(() -> new
+                        UsernameNotFoundException(
+                        "Invalid Credentials: Username or Password not found"));
     }
 }
