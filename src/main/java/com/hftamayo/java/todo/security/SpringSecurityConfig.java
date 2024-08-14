469a5acde7cad90db13f09102b3775ebd0849275
Herbert Tamayo <hftamayo@gmail.com>
2024-08-14 12:10:42 -0600

    removing userInfoProviderManager bean creation

--- src/main/java/com/hftamayo/java/todo/security/SpringSecurityConfig.java
+++ src/main/java/com/hftamayo/java/todo/security/SpringSecurityConfig.java
@@ -1,17 +1,13 @@
 package com.hftamayo.java.todo.security;
 
-import com.hftamayo.java.todo.security.jwt.AuthenticationFilter;
-import com.hftamayo.java.todo.security.jwt.CustomTokenProvider;
 import com.hftamayo.java.todo.security.jwt.JwtConfig;
 import com.hftamayo.java.todo.security.managers.UserInfoProviderManager;
 import com.hftamayo.java.todo.services.UserService;
-import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -28,26 +24,18 @@ public class SpringSecurityConfig {
 
     private UserService userService;
     private final PasswordEncoder passwordEncoder;
+    private final UserInfoProviderManager userInfoProviderManager;
 
     @Autowired
-    public SpringSecurityConfig(PasswordEncoder passwordEncoder) {
+    public SpringSecurityConfig(PasswordEncoder passwordEncoder, UserInfoProviderManager userInfoProviderManager) {
         this.passwordEncoder = passwordEncoder;
-    }
-
-    @Autowired
-    public void setUserService(UserService userService) {
-        this.userService = userService;
-    }
-
-    @Bean
-    public UserInfoProviderManager userInfoProviderManager() {
-        return new UserInfoProviderManager(userService);
+        this.userInfoProviderManager = userInfoProviderManager;
     }
 
     @Bean
     public AuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsService(userInfoProviderManager()::getUserDetails);
+        authenticationProvider.setUserDetailsService(userInfoProviderManager::getUserDetails);
         authenticationProvider.setPasswordEncoder(passwordEncoder);
         return authenticationProvider;
     }
