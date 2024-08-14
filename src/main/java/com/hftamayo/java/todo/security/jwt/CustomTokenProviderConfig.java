c43a68537ff2655a321625ab46378527ef5b87db
Herbert Tamayo <hftamayo@gmail.com>
2024-08-14 11:09:43 -0600

    adding a constructor to inject deps to tokenProvider class

---
+++ src/main/java/com/hftamayo/java/todo/security/jwt/CustomTokenProviderConfig.java
@@ -0,0 +1,25 @@
+package com.hftamayo.java.todo.security.jwt;
+
+import com.hftamayo.java.todo.security.managers.UserInfoProviderManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CustomTokenProviderConfig {
+    private final JwtConfig jwtConfig;
+    private final UserInfoProviderManager userInfoProviderManager;
+
+    @Autowired
+    public CustomTokenProviderConfig(JwtConfig jwtConfig,
+                                     UserInfoProviderManager userInfoProviderManager) {
+        this.jwtConfig = jwtConfig;
+        this.userInfoProviderManager = userInfoProviderManager;
+    }
+
+
+    @Bean
+    public CustomTokenProvider customTokenProvider() {
+        return new CustomTokenProvider(jwtConfig, userInfoProviderManager);
+    }
+}
