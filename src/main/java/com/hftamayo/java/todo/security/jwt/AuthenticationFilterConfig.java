ef29100e99ee8c098a96eff82257f67f141fdea6
Herbert Tamayo <hftamayo@gmail.com>
2024-08-14 10:56:29 -0600

    adding a constructor to inject deps to its main class

--- src/main/java/com/hftamayo/java/todo/security/jwt/AuthenticationFilterConfig.java
+++ src/main/java/com/hftamayo/java/todo/security/jwt/AuthenticationFilterConfig.java
@@ -1,22 +1,22 @@
 package com.hftamayo.java.todo.security.jwt;
 
 import com.hftamayo.java.todo.security.managers.UserInfoProviderManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 
 @Configuration
 public class AuthenticationFilterConfig {
     private final UserInfoProviderManager userInfoProviderManager;
     private final CustomTokenProvider customTokenProvider;
 
+    @Autowired
     public AuthenticationFilterConfig(UserInfoProviderManager userInfoProviderManager, CustomTokenProvider customTokenProvider) {
         this.userInfoProviderManager = userInfoProviderManager;
         this.customTokenProvider = customTokenProvider;
     }
 
     @Bean
-    @Lazy
     public AuthenticationFilter authenticationFilter() {
         return new AuthenticationFilter(userInfoProviderManager, customTokenProvider);
     }
