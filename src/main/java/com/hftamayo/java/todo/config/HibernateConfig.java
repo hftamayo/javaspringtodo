f5267fe830edf07e9edf0045d1e902205d1827c3
Herbert Tamayo <hftamayo@gmail.com>
2024-08-16 14:51:53 -0600

    trying to figure out the enviro context

--- src/main/java/com/hftamayo/java/todo/config/HibernateConfig.java
+++ src/main/java/com/hftamayo/java/todo/config/HibernateConfig.java
@@ -1,17 +1,26 @@
 package com.hftamayo.java.todo.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.Environment;
+import org.springframework.core.env.PropertySource;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
 import javax.sql.DataSource;
+import java.util.Map;
 import java.util.Properties;
+import java.util.stream.Collectors;
 
 @Configuration
 public class HibernateConfig {
+    
+    private static final Logger logger = LoggerFactory.getLogger(HibernateConfig.class);
 
     @Autowired
     private Environment env;
@@ -36,11 +45,37 @@ public class HibernateConfig {
         return dataSource;
     }
 
+    public void displayEnvironmentProperties() {
+        if (env instanceof ConfigurableEnvironment) {
+            ConfigurableEnvironment configurableEnvironment = (ConfigurableEnvironment) env;
+            logger.info("Displaying environment properties:");
+            for (PropertySource<?> propertySource : configurableEnvironment.getPropertySources()) {
+                if (propertySource.getSource() instanceof Map) {
+                    Map<String, Object> source = (Map<String, Object>) propertySource.getSource();
+                    for (String key : source.keySet()) {
+                        logger.info("{}: {}", key, env.getProperty(key));
+                    }
+                }
+            }
+        } else {
+            logger.warn("Environment is not an instance of ConfigurableEnvironment");
+        }
+    }
+
     private Properties hibernateProperties() {
         Properties properties = new Properties();
+
         properties.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
         properties.put("spring.jpa.properties.hibernate.show_sql", env.getProperty("hibernate.show_sql"));
         properties.put("spring.jpa.properties.hibernate.format_sql", env.getProperty("hibernate.format_sql"));
+
+        logger.info("hibernate properties: {}", properties);
+
         return properties;
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> displayEnvironmentProperties();
     }
 }
