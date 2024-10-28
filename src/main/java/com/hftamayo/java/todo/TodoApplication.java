1e0533e8a323c234578ef42a37f323d045ef9ed4
Herbert Tamayo <hftamayo@gmail.com>
2024-10-28 14:14:46 -0600

    using requiredArgsContructor on main function

--- src/main/java/com/hftamayo/java/todo/TodoApplication.java
+++ src/main/java/com/hftamayo/java/todo/TodoApplication.java
@@ -1,6 +1,7 @@
 package com.hftamayo.java.todo;
 
 import com.hftamayo.java.todo.services.DBConnectionService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,13 +10,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@RequiredArgsConstructor
 public class TodoApplication {
     private final DBConnectionService dbConnectionService;
-
-    @Autowired
-    public TodoApplication(DBConnectionService dbConnectionService) {
-        this.dbConnectionService = dbConnectionService;
-    }
 
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(TodoApplication.class, args);
