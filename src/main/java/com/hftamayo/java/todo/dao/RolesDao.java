96a7e2fed1c4fd07f2938a7163657e52dc3aa010
Herbert Tamayo <hftamayo@gmail.com>
2024-11-13 20:35:53 -0600

    removing spaces

--- src/main/java/com/hftamayo/java/todo/dao/RolesDao.java
+++ src/main/java/com/hftamayo/java/todo/dao/RolesDao.java
@@ -10,9 +10,6 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
