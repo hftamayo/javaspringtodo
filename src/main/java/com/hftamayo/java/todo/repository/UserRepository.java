50362577541720b5558333b292b7899d27b0a67c
Herbert Tamayo <hftamayo@gmail.com>
2024-11-16 22:56:08 -0600

    adding user repo class part2

--- src/main/java/com/hftamayo/java/todo/repository/UserRepository.java
+++ src/main/java/com/hftamayo/java/todo/repository/UserRepository.java
@@ -2,19 +2,18 @@ package com.hftamayo.java.todo.repository;
 
 import com.hftamayo.java.todo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
     @Override
     List<User> findAll();
     Optional<User> findUserById(long id);
+    Optional<User> findUserByEmail(String email);
     Optional<User> findByUsername(String username);
     void deleteUserById(long id);
-
-    Boolean existsByUsername(String username);
-    Boolean existsByEmail(String email);
 }
