764cf96a75d73a1340b58d5954d31e62c3fa0507
Herbert Tamayo <hftamayo@gmail.com>
2024-11-26 14:24:52 -0600

    commenting an int test just for reference

--- src/test/java/com/hftamayo/java/todo/Controller/TaskControllerITV1.java
+++ src/test/java/com/hftamayo/java/todo/Controller/TaskControllerITV1.java
@@ -27,114 +27,114 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import java.time.*;
 
-@WebMvcTest
-
-public class TaskControllerITV1 {
-
-    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
-    @Autowired
-    private MockMvc mockMvc;
-
-    @MockBean
-    private TaskService taskService;
-
-    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
-    @Autowired
-    private ObjectMapper objectMapper;
-
-    @Test
-    @DisplayName("get all available Tasks")
-    public void givenListOfTasks_whenGetAllTasks_thenReturnTasksList() throws Exception {
-
-        List<Task> listOfTasks = new ArrayList<>();
-        listOfTasks.add(Task.builder()
-                .title("listen YT Playlist")
-                .description("R & B PlayList")
-                .dateAdded(LocalDateTime.now())
-                .dateUpdated(LocalDateTime.now())
-                .status(true)
-                .build());
-        listOfTasks.add(Task.builder()
-                .title("listen Spotify Playlist")
-                .description("New Age PlayList")
-                .dateAdded(LocalDateTime.now())
-                .dateUpdated(LocalDateTime.now())
-                .status(true)
-                .build());
-        given(taskService.getTasks()).willReturn(listOfTasks);
-
-        ResultActions response = mockMvc.perform(get("/tasks"));
-
-        response.andExpect(status().isOk())
-                .andDo(print())
-                .andExpect(jsonPath("$.size()",
-                        is(listOfTasks.size())));
-
-    }
-
-    @Test
-    @DisplayName("insert a non-existent task")
-    public void givenTaskObject_whenCreateTask_thenReturnSavedTask() throws Exception{
-        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy hh:mm");
-        Task task = Task.builder()
-                .title("Go to the Medician")
-                .description("keep working on your health")
-                .dateAdded(LocalDateTime.of(2023, 07, 01, 12, 0))
-                .dateUpdated(LocalDateTime.of(2023, 07, 01, 12, 0))
-                .status(true)
-                .build();
-        given(taskService.saveTask(any(Task.class)))
-                .willAnswer((invocation) -> invocation.getArgument(0));
-
-        ResultActions response = mockMvc.perform(post("/savetask")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(task)));
-
-        response.andDo(print()).
-                andExpect(status().isCreated())
-                .andExpect(jsonPath("$.title", is(task.getTitle())))
-                .andExpect(jsonPath("$.description", is(task.getDescription())))
-                .andExpect(jsonPath("$.dateAdded", is(task.getDateAdded().format(dateFormatter))))
-                .andExpect(jsonPath("$.dateUpdated", is(task.getDateUpdated().format(dateFormatter))))
-                .andExpect(jsonPath("$.status", is(task.isStatus())))
-        ;
-    }
-
-    @Test
-    @DisplayName("update a task successfully")
-    public void givenUpdatedTask_whenUpdateTask_thenReturnUpdateTaskObject() throws Exception{
-        long taskId = 1L;
-        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy hh:mm");
-
-        Task savedTask = Task.builder()
-                .title("Go to the Medician")
-                .description("keep working on your health")
-                .dateAdded(LocalDateTime.of(2023, 07, 01, 12, 0))
-                .dateUpdated(LocalDateTime.of(2023, 07, 01, 12, 0))
-                .status(true)
-                .build();
-
-        Task updatedTask = Task.builder()
-                .title("Go to the Dentist")
-                .description("dont forget to keep calm")
-                .dateAdded(LocalDateTime.of(2023, 07, 01, 12, 0))
-                .dateUpdated(LocalDateTime.of(2023, 07, 01, 14, 0))
-                .status(true)
-                .build();
-        given(taskService.getTaskById(taskId)).willReturn(savedTask);
-        given(taskService.updateTask(eq(1), any(Task.class)))
-                .willAnswer((invocation)-> invocation.getArgument(0));
-
-        ResultActions response = mockMvc.perform(put("/updatetask/{taskId}", taskId)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(updatedTask)));
-
-        response.andExpect(status().isOk())
-                .andDo(print());
-//                .andExpect(jsonPath("$.title", is(updatedTask.getTitle())))
-//                .andExpect(jsonPath("$.description", is(updatedTask.getDescription())))
-//                .andExpect(jsonPath("$.dateUpdated", is(updatedTask.getDateUpdated())));
-    }
+//@WebMvcTest
+//
+//public class TaskControllerITV1 {
+//
+//    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+//    @Autowired
+//    private MockMvc mockMvc;
+//
+//    @MockBean
+//    private TaskService taskService;
+//
+//    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+//    @Autowired
+//    private ObjectMapper objectMapper;
+//
+//    @Test
+//    @DisplayName("get all available Tasks")
+//    public void givenListOfTasks_whenGetAllTasks_thenReturnTasksList() throws Exception {
+//
+//        List<Task> listOfTasks = new ArrayList<>();
+//        listOfTasks.add(Task.builder()
+//                .title("listen YT Playlist")
+//                .description("R & B PlayList")
+//                .dateAdded(LocalDateTime.now())
+//                .dateUpdated(LocalDateTime.now())
+//                .status(true)
+//                .build());
+//        listOfTasks.add(Task.builder()
+//                .title("listen Spotify Playlist")
+//                .description("New Age PlayList")
+//                .dateAdded(LocalDateTime.now())
+//                .dateUpdated(LocalDateTime.now())
+//                .status(true)
+//                .build());
+//        given(taskService.getTasks()).willReturn(listOfTasks);
+//
+//        ResultActions response = mockMvc.perform(get("/tasks"));
+//
+//        response.andExpect(status().isOk())
+//                .andDo(print())
+//                .andExpect(jsonPath("$.size()",
+//                        is(listOfTasks.size())));
+//
+//    }
+//
+//    @Test
+//    @DisplayName("insert a non-existent task")
+//    public void givenTaskObject_whenCreateTask_thenReturnSavedTask() throws Exception{
+//        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy hh:mm");
+//        Task task = Task.builder()
+//                .title("Go to the Medician")
+//                .description("keep working on your health")
+//                .dateAdded(LocalDateTime.of(2023, 07, 01, 12, 0))
+//                .dateUpdated(LocalDateTime.of(2023, 07, 01, 12, 0))
+//                .status(true)
+//                .build();
+//        given(taskService.saveTask(any(Task.class)))
+//                .willAnswer((invocation) -> invocation.getArgument(0));
+//
+//        ResultActions response = mockMvc.perform(post("/savetask")
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .content(objectMapper.writeValueAsString(task)));
+//
+//        response.andDo(print()).
+//                andExpect(status().isCreated())
+//                .andExpect(jsonPath("$.title", is(task.getTitle())))
+//                .andExpect(jsonPath("$.description", is(task.getDescription())))
+//                .andExpect(jsonPath("$.dateAdded", is(task.getDateAdded().format(dateFormatter))))
+//                .andExpect(jsonPath("$.dateUpdated", is(task.getDateUpdated().format(dateFormatter))))
+//                .andExpect(jsonPath("$.status", is(task.isStatus())))
+//        ;
+//    }
+//
+//    @Test
+//    @DisplayName("update a task successfully")
+//    public void givenUpdatedTask_whenUpdateTask_thenReturnUpdateTaskObject() throws Exception{
+//        long taskId = 1L;
+//        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy hh:mm");
+//
+//        Task savedTask = Task.builder()
+//                .title("Go to the Medician")
+//                .description("keep working on your health")
+//                .dateAdded(LocalDateTime.of(2023, 07, 01, 12, 0))
+//                .dateUpdated(LocalDateTime.of(2023, 07, 01, 12, 0))
+//                .status(true)
+//                .build();
+//
+//        Task updatedTask = Task.builder()
+//                .title("Go to the Dentist")
+//                .description("dont forget to keep calm")
+//                .dateAdded(LocalDateTime.of(2023, 07, 01, 12, 0))
+//                .dateUpdated(LocalDateTime.of(2023, 07, 01, 14, 0))
+//                .status(true)
+//                .build();
+//        given(taskService.getTaskById(taskId)).willReturn(savedTask);
+//        given(taskService.updateTask(eq(1), any(Task.class)))
+//                .willAnswer((invocation)-> invocation.getArgument(0));
+//
+//        ResultActions response = mockMvc.perform(put("/updatetask/{taskId}", taskId)
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .content(objectMapper.writeValueAsString(updatedTask)));
+//
+//        response.andExpect(status().isOk())
+//                .andDo(print());
+////                .andExpect(jsonPath("$.title", is(updatedTask.getTitle())))
+////                .andExpect(jsonPath("$.description", is(updatedTask.getDescription())))
+////                .andExpect(jsonPath("$.dateUpdated", is(updatedTask.getDateUpdated())));
+//    }
 
 //    @Test
 //    @DisplayName("update a task and return 404")
@@ -169,17 +169,17 @@ public class TaskControllerITV1 {
 //
 //    }
 
-    @Test
-    @DisplayName("delete a task successfully")
-    public void givenTaskId_whenDeleteTask_thenReturnOK() throws Exception{
-        long taskId = 1L;
-        willDoNothing().given(taskService).deleteTask(taskId);
-
-        ResultActions response = mockMvc.perform(delete("/deletetask/{taskId}", taskId));
-
-        response.andExpect(status().isOk())
-                .andDo(print());
-    }
+//    @Test
+//    @DisplayName("delete a task successfully")
+//    public void givenTaskId_whenDeleteTask_thenReturnOK() throws Exception{
+//        long taskId = 1L;
+//        willDoNothing().given(taskService).deleteTask(taskId);
+//
+//        ResultActions response = mockMvc.perform(delete("/deletetask/{taskId}", taskId));
+//
+//        response.andExpect(status().isOk())
+//                .andDo(print());
+//    }
 
 //    @Test
 //    @DisplayName("get a task by id")
@@ -200,4 +200,4 @@ public class TaskControllerITV1 {
 //                .andExpect(jsonPath("$.description", is(task.getDescription())));
 //    }
 
-}
+//}
