62d09ffb1813a2bcd916568260bc06b799187851
Herbert Tamayo <hftamayo@gmail.com>
2024-11-28 15:22:15 -0600

    refactoring roleGrantedAuth class

--- src/main/java/com/hftamayo/java/todo/security/interfaces/RoleGrantedAuthority.java
+++ src/main/java/com/hftamayo/java/todo/security/interfaces/RoleGrantedAuthority.java
@@ -3,12 +3,12 @@ package com.hftamayo.java.todo.security.interfaces;
 import com.hftamayo.java.todo.entity.Roles;
 import org.springframework.security.core.GrantedAuthority;
 
+@Getter
+@RequiredArgsConstructor
+@EqualsAndHashCode
+@ToString
 public class RoleGrantedAuthority implements GrantedAuthority {
     private final Roles role;
-
-    public RoleGrantedAuthority(Roles role) {
-        this.role = role;
-    }
 
     @Override
     public String getAuthority() {
