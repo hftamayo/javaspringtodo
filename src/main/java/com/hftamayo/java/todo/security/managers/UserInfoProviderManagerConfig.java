709e4fb744059c9c49b4018acbfb688f39f988d7
Herbert Tamayo <hftamayo@gmail.com>
2024-11-29 15:06:37 -0600

    refactoring userInfoProviderMgerConfig

--- src/main/java/com/hftamayo/java/todo/security/managers/UserInfoProviderManagerConfig.java
+++ src/main/java/com/hftamayo/java/todo/security/managers/UserInfoProviderManagerConfig.java
@@ -6,13 +6,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class UserInfoProviderManagerConfig {
     private final UserService userService;
-
-    @Autowired
-    public UserInfoProviderManagerConfig(UserService userService) {
-        this.userService = userService;
-    }
 
     @Bean
     public UserInfoProviderManager userInfoProviderManager() {
