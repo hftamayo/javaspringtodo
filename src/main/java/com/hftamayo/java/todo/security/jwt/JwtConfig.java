e3ba07d059bf90b6dfee21d2e65d4438e9244e1f
Herbert Tamayo <hftamayo@gmail.com>
2024-11-29 14:33:14 -0600

    refactoring JwtConfig

--- src/main/java/com/hftamayo/java/todo/security/jwt/JwtConfig.java
+++ src/main/java/com/hftamayo/java/todo/security/jwt/JwtConfig.java
@@ -5,7 +5,10 @@ import io.jsonwebtoken.security.Keys;
 
 import javax.crypto.SecretKey;
 
+@Configuration
 public class JwtConfig {
+
+    @Bean
     public SecretKey generateKey() {
         return Keys.secretKeyFor(SignatureAlgorithm.HS512);
     }
