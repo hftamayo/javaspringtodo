bb65e557094269abf373ec45fc3a344efc021e12
Herbert Tamayo <hftamayo@gmail.com>
2024-12-02 13:42:51 -0600

    updating imports

--- src/main/java/com/hftamayo/java/todo/security/managers/UserInfoProviderManager.java
+++ src/main/java/com/hftamayo/java/todo/security/managers/UserInfoProviderManager.java
@@ -5,6 +5,7 @@ import com.hftamayo.java.todo.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
--- src/main/java/com/hftamayo/java/todo/security/managers/UserInfoProviderManagerConfig.java
+++ src/main/java/com/hftamayo/java/todo/security/managers/UserInfoProviderManagerConfig.java
@@ -1,6 +1,7 @@
 package com.hftamayo.java.todo.security.managers;
 
 import com.hftamayo.java.todo.services.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
