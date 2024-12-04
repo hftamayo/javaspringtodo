8e857db82c5a0bb401a4094a1b28c39513e1e98f
Herbert Tamayo <hftamayo@gmail.com>
2024-12-04 14:02:47 -0600

    removing to createCustomTokenProdiver method

--- src/main/java/com/hftamayo/java/todo/security/jwt/CustomTokenProviderConfig.java
+++ src/main/java/com/hftamayo/java/todo/security/jwt/CustomTokenProviderConfig.java
@@ -2,7 +2,6 @@ package com.hftamayo.java.todo.security.jwt;
 
 import com.hftamayo.java.todo.security.managers.UserInfoProviderManager;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,7 +12,7 @@ public class CustomTokenProviderConfig {
     private final UserInfoProviderManager userInfoProviderManager;
 
     @Bean
-    public CustomTokenProvider customTokenProvider() {
+    public CustomTokenProvider createCustomTokenProvider() {
         return new CustomTokenProvider(jwtConfig, userInfoProviderManager);
     }
 }
