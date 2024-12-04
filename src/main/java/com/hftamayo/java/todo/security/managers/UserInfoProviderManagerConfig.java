269fbb792eb33793aa3dfc7c19b7f693d9d6ca2b
Herbert Tamayo <hftamayo@gmail.com>
2024-12-04 14:05:08 -0600

    removing to createUserInfoProviderManager method

--- src/main/java/com/hftamayo/java/todo/security/managers/UserInfoProviderManagerConfig.java
+++ src/main/java/com/hftamayo/java/todo/security/managers/UserInfoProviderManagerConfig.java
@@ -2,7 +2,6 @@ package com.hftamayo.java.todo.security.managers;
 
 import com.hftamayo.java.todo.services.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,7 +11,8 @@ public class UserInfoProviderManagerConfig {
     private final UserService userService;
 
     @Bean
-    public UserInfoProviderManager userInfoProviderManager() {
+    public UserInfoProviderManager createUserInfoProviderManager() {
+
         return new UserInfoProviderManager(userService);
     }
 }
