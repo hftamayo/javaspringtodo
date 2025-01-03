fb18395a35f024d37832f7be84b1046d36366479
Herbert Tamayo <hftamayo@gmail.com>
2025-01-03 14:29:41 -0600

    improving the output of User object wrapper in UserDetails pre-built class

--- src/main/java/com/hftamayo/java/todo/security/managers/UserInfoProviderManager.java
+++ src/main/java/com/hftamayo/java/todo/security/managers/UserInfoProviderManager.java
@@ -1,5 +1,6 @@
 package com.hftamayo.java.todo.security.managers;
 
+import com.hftamayo.java.todo.entity.User;
 import com.hftamayo.java.todo.security.interfaces.UserInfoProvider;
 import com.hftamayo.java.todo.services.UserService;
 import lombok.RequiredArgsConstructor;
@@ -7,16 +8,23 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-@RequiredArgsConstructor
 @Service
+@RequiredArgsConstructor
 public class UserInfoProviderManager implements UserInfoProvider {
     private final UserService userService;
 
     @Override
     public UserDetails getUserDetails(String email) {
-        return userService.getUserByEmail(email)
-                .orElseThrow(() -> new
-                        UsernameNotFoundException(
-                        "Invalid Credentials: Email or Password not found"));
+        User user = userService.getUserByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Invalid Credentials: Email or Password not found"));
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                user.isEnabled(),
+                user.isAccountNonExpired(),
+                user.isCredentialsNonExpired(),
+                user.isAccountNonLocked(),
+                user.getAuthorities()
+        );
     }
 }
