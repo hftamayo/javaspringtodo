7330db5bb3539aad7881f86a5dbddf6a17c4717e
Herbert Tamayo <hftamayo@gmail.com>
2025-02-05 14:21:52 -0600

    updating tasks endpoint names

--- src/main/java/com/hftamayo/java/todo/controller/TaskController.java
+++ src/main/java/com/hftamayo/java/todo/controller/TaskController.java
@@ -17,42 +17,42 @@ import java.util.Optional;
 
 @AllArgsConstructor
 @RestController
-@RequestMapping("/api/todos")
+@RequestMapping("/api/tasks")
 public class TaskController {
     private final TaskService taskService;
     private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
 
-    @GetMapping(value = "/alltasks")
+    @GetMapping(value = "/list")
     @ResponseStatus(HttpStatus.OK)
     public List<TaskResponseDto> getTasks() {
         return taskService.getTasks();
     }
 
-    @GetMapping(value = "/gettaskbyid/{taskId}")
+    @GetMapping(value = "/task/{taskId}")
     @ResponseStatus(HttpStatus.OK)
     public Optional<TaskResponseDto> getTask(@PathVariable long taskId) {
         return taskService.getTask(taskId);
     }
 
-    @GetMapping(value = "/gettaskbycriteria/{criteria}/{value}")
+    @GetMapping(value = "/taskbc/{criteria}/{value}")
     @ResponseStatus(HttpStatus.OK)
     public Optional<List<TaskResponseDto>> getTaskByCriteria(@PathVariable String criteria, @PathVariable String value) {
         return taskService.getTaskByCriteria(criteria, value);
     }
 
-    @GetMapping(value = "/gettaskbycriterias/{criteria}/{value}/{criteria2}/{value2}")
+    @GetMapping(value = "/taskbcs/{criteria}/{value}/{criteria2}/{value2}")
     @ResponseStatus(HttpStatus.OK)
     public Optional<List<TaskResponseDto>> getTaskByCriterias(@PathVariable String criteria, @PathVariable String value, @PathVariable String criteria2, @PathVariable String value2) {
         return taskService.getTaskByCriterias(criteria, value, criteria2, value2);
     }
 
-    @PostMapping(value = "/savetask")
+    @PostMapping(value = "/create")
     @ResponseStatus(HttpStatus.CREATED)
     public TaskResponseDto saveTask(@RequestBody Task task) {
         return taskService.saveTask(task);
     }
 
-    @PutMapping(value = "/updatetask/{taskId}")
+    @PutMapping(value = "/update/{taskId}")
     public TaskResponseDto updateTask(@PathVariable long taskId, @RequestBody Task task) {
         try {
             return taskService.updateTask(taskId, task);
@@ -61,7 +61,7 @@ public class TaskController {
         }
     }
 
-    @DeleteMapping(value = "/deletetask/{taskId}")
+    @DeleteMapping(value = "/delete/{taskId}")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<?> deleteTask(@PathVariable long taskId) {
         try {
