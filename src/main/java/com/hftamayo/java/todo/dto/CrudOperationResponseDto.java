3853a7a8e831505eceee8e4471608ee1c1455afe
Herbert Tamayo <hftamayo@gmail.com>
2025-02-28 15:36:25 -0600

    updating generic crudResponse object for handling every case derived for persistence ops

--- src/main/java/com/hftamayo/java/todo/dto/CrudOperationResponseDto.java
+++ src/main/java/com/hftamayo/java/todo/dto/CrudOperationResponseDto.java
@@ -5,17 +5,31 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
-@AllArgsConstructor
+import java.util.Map;
+
 @Builder
 @Getter
 @Setter
-public class CrudOperationResponseDto {
+public class CrudOperationResponseDto<T> {
     private int code;
     private String resultMessage;
-    private Object data;
+    private T data;
+    private Map<String, T> dataMap;
 
     public CrudOperationResponseDto(int code, String resultMessage) {
         this.code = code;
         this.resultMessage = resultMessage;
+    }
+
+    public CrudOperationResponseDto(int code, String resultMessage, T data) {
+        this.code = code;
+        this.resultMessage = resultMessage;
+        this.data = data;
+    }
+
+    public CrudOperationResponseDto(int code, String resultMessage, Map<String, T> dataMap) {
+        this.code = code;
+        this.resultMessage = resultMessage;
+        this.dataMap = dataMap;
     }
 }
