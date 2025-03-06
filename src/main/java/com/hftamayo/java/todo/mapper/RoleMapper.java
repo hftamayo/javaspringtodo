70461ea32aa0f6e562003e1ebf13a9ab83d61d58
Herbert Tamayo <hftamayo@gmail.com>
2025-03-06 12:08:32 -0600

    improving roleMapper functionality

--- src/main/java/com/hftamayo/java/todo/mapper/RoleMapper.java
+++ src/main/java/com/hftamayo/java/todo/mapper/RoleMapper.java
@@ -6,20 +6,24 @@ import com.hftamayo.java.todo.entity.Roles;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Component
 public class RoleMapper {
     public RolesResponseDto toRolesResponseDto(Roles roles) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
+        String formattedDate = roles.getDateAdded().format(formatter);
+
         return new RolesResponseDto(
                 roles.getId(),
                 roles.getRoleEnum().name(),
                 roles.getDescription(),
                 roles.isStatus(),
-                roles.getDateAdded().toString()
+                formattedDate
         );
     }
 
-public Roles toEntity(RolesResponseDto rolesResponseDto) {
+    public Roles toEntity(RolesResponseDto rolesResponseDto) {
         Roles roles = new Roles();
         roles.setId(rolesResponseDto.getId());
         roles.setRoleEnum(ERole.valueOf(rolesResponseDto.getRoleName()));
