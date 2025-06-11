566e54eb0d2d3e03d18647a9b5bafb05ca69b492
Herbert Tamayo <hftamayo@gmail.com>
2025-06-11 12:26:04 -0600

    finishing testCheckDBConnOnFailure test case

--- src/test/java/com/hftamayo/java/todo/services/impl/DBConnectionServiceImplTest.java
+++ src/test/java/com/hftamayo/java/todo/services/impl/DBConnectionServiceImplTest.java
@@ -2,11 +2,12 @@ package com.hftamayo.java.todo.services.impl;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import javax.sql.DataSource;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.sql.Connection;
 import java.sql.SQLException;
 
@@ -16,9 +17,30 @@ import static org.mockito.Mockito.*;
 @ExtendWith(MockitoExtension.class)
 public class DBConnectionServiceImplTest {
 
-    private DBConnectionServiceImpl dbConnectionService;
+    private TestableDBConnectionServiceImpl dbConnectionService;
     private DataSource dataSource;
     private Connection mockConnection;
+    private ByteArrayOutputStream outContent;
+    private PrintStream originalOut;
+
+    // Test-specific implementation that doesn't actually exit
+    private static class TestableDBConnectionServiceImpl extends DBConnectionServiceImpl {
+        private boolean shouldExit = false;
+
+        public TestableDBConnectionServiceImpl(DataSource dataSource) {
+            super(dataSource);
+        }
+
+        @Override
+        protected void handleDatabaseConnectionFailure() {
+            System.out.println("Unable to establish database connection. Application will stop.");
+            shouldExit = true;
+        }
+
+        public boolean shouldExit() {
+            return shouldExit;
+        }
+    }
 
     @BeforeEach
     public void setUp() throws SQLException {
@@ -30,7 +52,12 @@ public class DBConnectionServiceImplTest {
         when(dataSource.getConnection()).thenReturn(mockConnection);
         
         // Create service with mocked dependencies
-        dbConnectionService = new DBConnectionServiceImpl(dataSource);
+        dbConnectionService = new TestableDBConnectionServiceImpl(dataSource);
+
+        // Setup output capture
+        outContent = new ByteArrayOutputStream();
+        originalOut = System.out;
+        System.setOut(new PrintStream(outContent));
     }
 
     @Test
@@ -49,7 +76,6 @@ public class DBConnectionServiceImplTest {
     @Test
     public void testCheckDatabaseConnectionOnStartup_Failure() throws SQLException {
         // Arrange
-        //this case must check dbConn failure with a live db
         when(dataSource.getConnection()).thenThrow(new SQLException("Database connection failed"));
 
         // Act
@@ -57,7 +83,9 @@ public class DBConnectionServiceImplTest {
 
         // Assert
         assertFalse(dbConnectionService.isDatabaseOnline());
-        verify(dataSource, times(1)).getConnection();
+        assertTrue(dbConnectionService.shouldExit());
+        assertTrue(outContent.toString().contains("Unable to establish database connection. Application will stop."));
+        verify(dataSource, times(3)).getConnection();
     }
 
     @Test
