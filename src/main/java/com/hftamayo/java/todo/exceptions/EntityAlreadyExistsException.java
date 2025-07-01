2a9acb8a71ae1d1480ec0902610956c08f09e83c
Herbert Tamayo <hftamayo@gmail.com>
2025-07-01 13:10:02 -0600

    deprecating existing classes from exceptions package

--- src/main/java/com/hftamayo/java/todo/exceptions/EntityAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.hftamayo.java.todo.exceptions;
-
-public class EntityAlreadyExistsException extends RuntimeException {
-    public EntityAlreadyExistsException(String message) {
-        super(message);
-    }
-}
