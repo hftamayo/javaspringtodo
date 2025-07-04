97b1427ad462da66923dce7755b2a74627d70d9b
Herbert Tamayo <hftamayo@gmail.com>
2025-07-04 14:49:27 -0600

    using RequiredArgsCons annotation to work with global var

--- src/main/java/com/hftamayo/java/todo/controller/AuthController.java
+++ src/main/java/com/hftamayo/java/todo/controller/AuthController.java
@@ -9,6 +9,7 @@ import com.hftamayo.java.todo.services.AuthService;
 import com.hftamayo.java.todo.services.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -18,7 +19,7 @@ import org.springframework.beans.factory.annotation.Value;
 
 import java.util.Collections;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
@@ -26,26 +27,23 @@ public class AuthController {
     private AuthService authService;
     private UserService userService;
     
-    @Value("${app.environment:production}")
+    @Value("${app.environment}")
     private String environment;
-    
-    @Value("${app.debug.jwt:false}")
-    private boolean debugJwt;
 
     @PostMapping("/login")
     public ResponseEntity<String> authenticate(@RequestBody LoginRequestDto loginRequestDto) {
-        logger.info("Login attempt: " + loginRequestDto.getEmail());
+        logger.info("Login attempt: {}", loginRequestDto.getEmail());
         try {
             ActiveSessionResponseDto activeSessionResponseDto = authService.login(loginRequestDto);
 
-            logger.info("Login successful, welcome: " + loginRequestDto.getEmail() +
-                    "!, assigned roles: " + activeSessionResponseDto.getRoles());
+            logger.info("Login successful, welcome: {}!, assigned roles: {}",
+                    loginRequestDto.getEmail(), activeSessionResponseDto.getRoles());
 
             // Only log JWT details in development or when explicitly enabled
-            if ("development".equals(environment) || debugJwt) {
-                logger.info("token: " + activeSessionResponseDto.getAccessToken() + " type: " +
-                        activeSessionResponseDto.getTokenType() + " expires in: " +
-                        activeSessionResponseDto.getExpiresIn() + " hours");
+            if ("development".equals(environment)) {
+                logger.info("token: {} type: {} expires in: {} hours",
+                        activeSessionResponseDto.getAccessToken(), activeSessionResponseDto.getTokenType(),
+                        activeSessionResponseDto.getExpiresIn());
             }
 
             return ResponseEntity.ok("LOGIN_SUCCESSFUL " + loginRequestDto.getEmail() + "!" +
@@ -67,9 +65,11 @@ public class AuthController {
         try {
             authService.logout(request);
             logger.info("Session destroyed, logout successful");
-            return ResponseEntity.ok(Collections.singletonMap("message", "Session destroyed as expected, logout successful, have a nice day"));
+            return ResponseEntity.ok(Collections
+                    .singletonMap("message", "Session destroyed as expected, logout successful, have a nice day"));
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred during logout: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("An error occurred during logout: " + e.getMessage());
         }
     }
 
