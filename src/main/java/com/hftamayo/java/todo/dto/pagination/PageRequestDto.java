48f96ee9f42d2598411d5ee3bf586c0ac1c1c253
Herbert Tamayo <hftamayo@gmail.com>
2025-07-08 13:34:45 -0600

    DTO classes for pagination

---
+++ src/main/java/com/hftamayo/java/todo/dto/pagination/PageRequestDto.java
@@ -0,0 +1,39 @@
+package com.hftamayo.java.todo.dto.pagination;
+
+public class PageRequestDto {
+    private int page = 0;
+    private int size = 10;
+    private String sort;
+
+    public PageRequestDto() {}
+
+    public PageRequestDto(int page, int size, String sort) {
+        this.page = page;
+        this.size = size;
+        this.sort = sort;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public String getSort() {
+        return sort;
+    }
+
+    public void setSort(String sort) {
+        this.sort = sort;
+    }
+} 
