79f72874643b35f233bd28eaaa7addd6cfcfc529
Herbert Tamayo <hftamayo@gmail.com>
2025-07-25 12:00:09 -0600

    updating controllers using envelope response and applying responseUtil

--- src/main/java/com/hftamayo/java/todo/controller/RolesController.java
+++ src/main/java/com/hftamayo/java/todo/controller/RolesController.java
@@ -7,9 +7,11 @@ import com.hftamayo.java.todo.entity.Roles;
 import com.hftamayo.java.todo.services.RolesService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import com.hftamayo.java.todo.dto.pagination.PaginatedDataDto;
+import com.hftamayo.java.todo.utilities.endpoints.ResponseUtil;
 
 @AllArgsConstructor
 @RestController
@@ -18,38 +20,78 @@ public class RolesController {
     private final RolesService rolesService;
 
     @GetMapping(value = "/list")
-    @ResponseStatus(HttpStatus.OK)
-    public EndpointResponseDto<PaginatedDataDto<RolesResponseDto>> getRoles(
+    public ResponseEntity<EndpointResponseDto<?>> getRoles(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "#{${pagination.default-page-size:10}}") int size,
             @RequestParam(required = false) String sort) {
-        PageRequestDto pageRequestDto = new PageRequestDto(page, size, sort);
-        PaginatedDataDto<RolesResponseDto> paginatedData = rolesService.getPaginatedRoles(pageRequestDto);
-        return new EndpointResponseDto<>(200, "OPERATION_SUCCESS", paginatedData);
+        try {
+            PageRequestDto pageRequestDto = new PageRequestDto(page, size, sort);
+            PaginatedDataDto<RolesResponseDto> paginatedData = rolesService.getPaginatedRoles(pageRequestDto);
+            EndpointResponseDto<PaginatedDataDto<RolesResponseDto>> response =
+                    ResponseUtil.successResponse(paginatedData, "OPERATION_SUCCESS");
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return new ResponseEntity<>(
+                ResponseUtil.errorResponse
+                        (HttpStatus.INTERNAL_SERVER_ERROR, "Failed to fetch roles list", e),
+                HttpStatus.INTERNAL_SERVER_ERROR
+            );
+        }
     }
 
     @GetMapping(value = "/rolebn/{roleName}")
-    @ResponseStatus(HttpStatus.OK)
-    public EndpointResponseDto<RolesResponseDto> getRoleByName(@PathVariable String name) {
-        return rolesService.getRoleByName(name);
+    public ResponseEntity<EndpointResponseDto<?>> getRoleByName(@PathVariable String name) {
+        try {
+            EndpointResponseDto<RolesResponseDto> response = rolesService.getRoleByName(name);
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return new ResponseEntity<>(
+                ResponseUtil.errorResponse(HttpStatus.NOT_FOUND, "Role not found", e),
+                HttpStatus.NOT_FOUND
+            );
+        }
     }
 
     @PostMapping(value = "/create")
-    @ResponseStatus(HttpStatus.CREATED)
-    public EndpointResponseDto<RolesResponseDto> saveRole(@RequestBody Roles role) {
-        return rolesService.saveRole(role);
+    public ResponseEntity<EndpointResponseDto<?>> saveRole(@RequestBody Roles role) {
+        try {
+            EndpointResponseDto<RolesResponseDto> response =
+                    ResponseUtil.createdResponse(rolesService.saveRole(role).getData(), "ROLE_CREATED");
+            return new ResponseEntity<>(response, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(
+                ResponseUtil.errorResponse(HttpStatus.BAD_REQUEST, "Failed to create role", e),
+                HttpStatus.BAD_REQUEST
+            );
+        }
     }
 
     @PatchMapping(value = "/update/{roleId}")
-    @ResponseStatus(HttpStatus.OK)
-    public EndpointResponseDto<RolesResponseDto> updateRole(@PathVariable long roleId, @RequestBody Roles role) {
-            return rolesService.updateRole(roleId, role);
+    public ResponseEntity<EndpointResponseDto<?>> updateRole(@PathVariable long roleId, @RequestBody Roles role) {
+        try {
+            EndpointResponseDto<RolesResponseDto> response =
+                    ResponseUtil.successResponse(rolesService.updateRole(roleId, role).getData(), "ROLE_UPDATED");
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return new ResponseEntity<>(
+                ResponseUtil.errorResponse(HttpStatus.BAD_REQUEST, "Failed to update role", e),
+                HttpStatus.BAD_REQUEST
+            );
+        }
     }
 
     @DeleteMapping(value = "/delete/{roleId}")
     @PreAuthorize("hasRole('ADMIN')")
-    @ResponseStatus(HttpStatus.OK)
-    public EndpointResponseDto<RolesResponseDto> deleteRole(@PathVariable long roleId) {
-            return rolesService.deleteRole(roleId);
+    public ResponseEntity<EndpointResponseDto<?>> deleteRole(@PathVariable long roleId) {
+        try {
+            rolesService.deleteRole(roleId);
+            EndpointResponseDto<Void> response = ResponseUtil.successResponse(null, "ROLE_DELETED");
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return new ResponseEntity<>(
+                ResponseUtil.errorResponse(HttpStatus.BAD_REQUEST, "Failed to delete role", e),
+                HttpStatus.BAD_REQUEST
+            );
+        }
     }
 }
--- src/main/java/com/hftamayo/java/todo/controller/TaskController.java
+++ src/main/java/com/hftamayo/java/todo/controller/TaskController.java
@@ -3,15 +3,16 @@ package com.hftamayo.java.todo.controller;
 import com.hftamayo.java.todo.dto.EndpointResponseDto;
 import com.hftamayo.java.todo.dto.pagination.PageRequestDto;
 import com.hftamayo.java.todo.dto.pagination.PaginatedDataDto;
-import com.hftamayo.java.todo.dto.roles.RolesResponseDto;
 import com.hftamayo.java.todo.entity.Task;
 import com.hftamayo.java.todo.services.TaskService;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.hftamayo.java.todo.dto.task.TaskResponseDto;
+import com.hftamayo.java.todo.utilities.endpoints.ResponseUtil;
 
 @AllArgsConstructor
 @RestController
@@ -21,49 +22,99 @@ public class TaskController {
     private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
 
     @GetMapping(value = "/list")
-    @ResponseStatus(HttpStatus.OK)
-    public EndpointResponseDto<PaginatedDataDto<TaskResponseDto>> getTasks(
+    public ResponseEntity<EndpointResponseDto<?>> getTasks(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "#{${pagination.default-page-size:10}}") int size,
             @RequestParam(required = false) String sort) {
-        PageRequestDto pageRequestDto = new PageRequestDto(page, size, sort);
-        PaginatedDataDto<TaskResponseDto> paginatedData = taskService.getPaginatedTasks(pageRequestDto);
-        return new EndpointResponseDto<>(200, "OPERATION_SUCCESS", paginatedData);
+        try {
+            PageRequestDto pageRequestDto = new PageRequestDto(page, size, sort);
+            PaginatedDataDto<TaskResponseDto> paginatedData = taskService.getPaginatedTasks(pageRequestDto);
+            EndpointResponseDto<PaginatedDataDto<TaskResponseDto>> response = ResponseUtil.successResponse(paginatedData, "OPERATION_SUCCESS");
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return new ResponseEntity<>(
+                ResponseUtil.errorResponse(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to fetch tasks list", e),
+                HttpStatus.INTERNAL_SERVER_ERROR
+            );
+        }
     }
 
     @GetMapping(value = "/task/{taskId}")
-    @ResponseStatus(HttpStatus.OK)
-    public EndpointResponseDto<TaskResponseDto> getTask(@PathVariable long taskId) {
-        return taskService.getTask(taskId);
+    public ResponseEntity<EndpointResponseDto<?>> getTask(@PathVariable long taskId) {
+        try {
+            EndpointResponseDto<TaskResponseDto> response = taskService.getTask(taskId);
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return new ResponseEntity<>(
+                ResponseUtil.errorResponse(HttpStatus.NOT_FOUND, "Task not found", e),
+                HttpStatus.NOT_FOUND
+            );
+        }
     }
 
     @GetMapping(value = "/taskbc/{criteria}/{value}")
-    @ResponseStatus(HttpStatus.OK)
-    public EndpointResponseDto<TaskResponseDto> getTaskByCriteria(@PathVariable String criteria, @PathVariable String value) {
-        return taskService.getTaskByCriteria(criteria, value);
+    public ResponseEntity<EndpointResponseDto<?>> getTaskByCriteria(@PathVariable String criteria, @PathVariable String value) {
+        try {
+            EndpointResponseDto<TaskResponseDto> response = taskService.getTaskByCriteria(criteria, value);
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return new ResponseEntity<>(
+                ResponseUtil.errorResponse(HttpStatus.NOT_FOUND, "Task not found by criteria", e),
+                HttpStatus.NOT_FOUND
+            );
+        }
     }
 
     @GetMapping(value = "/taskbcs/{criteria}/{value}/{criteria2}/{value2}")
-    @ResponseStatus(HttpStatus.OK)
-    public EndpointResponseDto<TaskResponseDto> getTaskByCriterias(@PathVariable String criteria, @PathVariable String value, @PathVariable String criteria2, @PathVariable String value2) {
-        return taskService.getTaskByCriterias(criteria, value, criteria2, value2);
+    public ResponseEntity<EndpointResponseDto<?>> getTaskByCriterias(@PathVariable String criteria, @PathVariable String value, @PathVariable String criteria2, @PathVariable String value2) {
+        try {
+            EndpointResponseDto<TaskResponseDto> response = taskService.getTaskByCriterias(criteria, value, criteria2, value2);
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return new ResponseEntity<>(
+                ResponseUtil.errorResponse(HttpStatus.NOT_FOUND, "Task not found by criterias", e),
+                HttpStatus.NOT_FOUND
+            );
+        }
     }
 
     @PostMapping(value = "/create")
-    @ResponseStatus(HttpStatus.CREATED)
-    public EndpointResponseDto<TaskResponseDto> saveTask(@RequestBody Task task) {
-        return taskService.saveTask(task);
+    public ResponseEntity<EndpointResponseDto<?>> saveTask(@RequestBody Task task) {
+        try {
+            EndpointResponseDto<TaskResponseDto> response = ResponseUtil.createdResponse(taskService.saveTask(task).getData(), "TASK_CREATED");
+            return new ResponseEntity<>(response, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(
+                ResponseUtil.errorResponse(HttpStatus.BAD_REQUEST, "Failed to create task", e),
+                HttpStatus.BAD_REQUEST
+            );
+        }
     }
 
     @PatchMapping(value = "/update/{taskId}")
-    @ResponseStatus(HttpStatus.OK)
-    public EndpointResponseDto<TaskResponseDto> updateTask(@PathVariable long taskId, @RequestBody Task task) {
-        return taskService.updateTask(taskId, task);
+    public ResponseEntity<EndpointResponseDto<?>> updateTask(@PathVariable long taskId, @RequestBody Task task) {
+        try {
+            EndpointResponseDto<TaskResponseDto> response = ResponseUtil.successResponse(taskService.updateTask(taskId, task).getData(), "TASK_UPDATED");
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return new ResponseEntity<>(
+                ResponseUtil.errorResponse(HttpStatus.BAD_REQUEST, "Failed to update task", e),
+                HttpStatus.BAD_REQUEST
+            );
+        }
     }
 
     @DeleteMapping(value = "/delete/{taskId}")
-    @ResponseStatus(HttpStatus.OK)
-    public EndpointResponseDto deleteTask(@PathVariable long taskId) {
-        return taskService.deleteTask(taskId);
+    public ResponseEntity<EndpointResponseDto<?>> deleteTask(@PathVariable long taskId) {
+        try {
+            taskService.deleteTask(taskId);
+            EndpointResponseDto<Void> response = ResponseUtil.successResponse(null, "TASK_DELETED");
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return new ResponseEntity<>(
+                ResponseUtil.errorResponse(HttpStatus.BAD_REQUEST, "Failed to delete task", e),
+                HttpStatus.BAD_REQUEST
+            );
+        }
     }
 }
