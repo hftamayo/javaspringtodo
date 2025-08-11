d3b7bcf7d86a2bfd61b03732c9bd4d65a9f56ee1
Herbert Tamayo <hftamayo@gmail.com>
2025-08-11 14:42:02 -0600

    config classes for versioning management

---
+++ src/main/java/com/hftamayo/java/todo/config/VersionInterceptor.java
@@ -0,0 +1,38 @@
+package com.hftamayo.java.todo.config;
+
+import com.hftamayo.java.todo.utilities.version.VersionConstants;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+/**
+ * Interceptor to automatically add version headers to API responses.
+ * Ensures all API responses include version information for client awareness.
+ */
+@Component
+@RequiredArgsConstructor
+public class VersionInterceptor implements HandlerInterceptor {
+    
+    private final VersionProperties versionProperties;
+    
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if (versionProperties.getApi().getHeaders().isEnabled()) {
+            // Add API version header
+            response.setHeader(
+                versionProperties.getApi().getHeaders().getVersionHeader(),
+                VersionConstants.getCurrentApiVersion()
+            );
+            
+            // Add build version header
+            response.setHeader(
+                versionProperties.getApi().getHeaders().getBuildHeader(),
+                VersionConstants.getFullVersion()
+            );
+        }
+        
+        return true;
+    }
+}
